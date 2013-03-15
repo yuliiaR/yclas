4586fc690b28bd488c556d11506ea9ade264c2a2
Chema <neo22s@gmaill.com>
2013-03-15 09:41:33 +0100

    installation files

--- install/index.php
+++ install/index.php
@@ -1,12 +1,116 @@
-<? 
-include 'checks.php';
-include 'functions.php';
-include 'header.php';?>
+<? include 'functions.php';?>
 
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en>"> <!--<![endif]-->
+<head>
+	<meta charset="utf8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-<?if ($succeed){?>
+	<title><?=(SAMBA)? 'Open Classifieds':'';?> <?=_("Installation");?></title>
+    <meta name="keywords" content="" >
+    <meta name="description" content="" >
+    <meta name="copyright" content="<?=(SAMBA)? 'Open Classifieds':'';?> <?=VERSION;?>" >
+	<meta name="author" content="<?=(SAMBA)? 'Open Classifieds':'';?>">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 
-<? include 'install.php';?>
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script type="text/javascript" src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>    <![endif]-->
+    
+    <link type="text/css" href="" rel="stylesheet" media="screen" />	
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+    </style>
+    
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>	
+	
+	<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+  
+	<div class="navbar navbar-fixed-top navbar-inverse">
+<div class="navbar-inner">
+<div class="container"><a class="brand"><?=(SAMBA)? 'Open Classifieds':'';?> <?=_("Installation");?></a>
+<div class="nav-collapse">
+
+<? if (SAMBA){?>
+<div class="btn-group pull-right">
+	<a class="btn btn-primary" href="http://open-classifieds.com/download/">
+		<i class="icon-shopping-cart icon-white"></i> We install it for you, Buy now!
+	</a>
+</div>
+<? }?>
+
+</div>
+<!--/.nav-collapse --></div>
+</div>
+</div>    <div class="container">
+	    <div class="row">
+	    
+		<div class="span3">
+	<div class="well sidebar-nav">
+	
+		<ul class="nav nav-list">
+			<li class="nav-header"><?=__("Requirements");?></li>
+			<li class="divider"></li>
+			
+			<li class="nav-header"><?=__("Server software");?></li>
+
+			<?foreach ($checks as $name => $values):
+				if ($values['mandatory'] == TRUE AND $values['result'] == FALSE)
+					$succeed = FALSE;
+
+				if ($values['result'] == FALSE)
+					$msg .= $values['message'].'<br>';
+			?>
+
+				<li><i class="icon-<?=($values['result'])?"ok":"remove"?>"></i> 
+					<?=$name?>
+				</li>
+			<?endforeach?>
+
+			<li class="divider"></li>
+			<li><a href="install/phpinfo.php"><?=__("PHP Info");?></a></li>
+			<li class="divider"></li>
+			<? if (SAMBA){?>
+			<li class="nav-header">Open Classifieds</li>
+			<li><a href="http://open-classifieds.com/themes/">Themes</a></li>
+			<li><a href="http://open-classifieds.com/download/">Support</a></li>
+			<li><a href="http://j.mp/ocdonate" target="_blank">
+					<img src="http://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" alt="">
+			</a></li>
+			<li class="divider"></li>
+			<? }?>
+		</ul>
+		<? if (SAMBA){?>
+		<a href="https://twitter.com/openclassifieds"
+				onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','http://twitter.com']);"
+				class="twitter-follow-button" data-show-count="false"
+				data-size="large">Follow @openclassifieds</a><br />
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		<? }?>
+		
+	</div>
+	<!--/.well -->
+</div>
+<!--/span-->	
+
+<div class="span9">
+<?
+if ($succeed)
+{
+	include 'install.php';?>
 
 
 
@@ -160,7 +264,8 @@ include 'header.php';?>
 <?
 }//if requirements succeed
 
-else {?>
+else //not succeed
+{?>
 
 <div class="alert alert-error"><?=$msg;?></div>
 <?hostingAd(); }?>
@@ -175,4 +280,22 @@ else {?>
 <?}?>
 
 
-<?include 'footer.php';?>
+</div><!--/span--> 
+		</div><!--/row-->
+		<hr>
+
+<footer>
+<p>
+&copy; 
+<?php if (SAMBA){?>
+	<a href="http://open-classifieds.com" title="Open Source PHP Classifieds">Open Classifieds</a> 2009 - 
+<?php }?>
+<?php echo date('Y')?>
+</p>
+</footer>    </div><!--/.fluid-container-->
+	
+	<!--[if lt IE 7 ]>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>		<script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
+	<![endif]-->
+  </body>
+</html>
--- install/install.php
+++ install/install.php
@@ -10,7 +10,7 @@ if ($_POST)
 	//check DB connection
 	$link = mysql_connect($_POST["DB_HOST"], $_POST["DB_USER"], $_POST["DB_PASS"]);
 	if (!$link) {
-		$error_msg.= T_("Cannot connect to server").' '. $_POST["DB_HOST"].' '. mysql_error();
+		$error_msg.= __("Cannot connect to server").' '. $_POST["DB_HOST"].' '. mysql_error();
 		$install = false;
 	}
 	
@@ -18,16 +18,16 @@ if ($_POST)
         if ($_POST["DB_NAME"]){
             $dbcheck = mysql_select_db($_POST["DB_NAME"]);
             if (!$dbcheck){
-            	 $error_msg.= T_("Database name").': ' . mysql_error();
+            	 $error_msg.= __("Database name").': ' . mysql_error();
             	 $install = false;
            }
         } else {
-    		$error_msg.= '<p>'.T_("No database name was given").'. '.T_("Available databases").":</p>\n";
+    		$error_msg.= '<p>'.__("No database name was given").'. '.__("Available databases").":</p>\n";
     		$db_list = @mysql_query("SHOW DATABASES");
 
     		$error_msg.= "<pre>\n";
 		if (!$db_list) {
-			$error_msg.= T_('Invalid query'). ":\n" . mysql_error();
+			$error_msg.= __('Invalid query'). ":\n" . mysql_error();
 		} else {
 			while ($row = mysql_fetch_assoc($db_list)) {
 				$error_msg.= $row['Database'] . "\n";
@@ -71,101 +71,6 @@ if ($_POST)
 	    else $apiKey="";
 	}
 
-///////////////////////////////////////////////////////
-	//create config.php
-	if ($install)
-	{
-		$config_content = "<?php\n//Open Classifieds v ".VERSION.' '.T_("Config").' '.date("d/m/Y G:i:s")."\n";
-		foreach  ($_POST AS $key => $value){
-			$config_content.="define('$key','$value');\n";		
-		}
-		$config_content.="define('CHARSET','UTF-8');
-define('LOCALE_EXT','');
-define('DEFAULT_THEME', 'minimalistic');
-define('THEME_SELECTOR',false);
-define('THEME_MOBILE',false);
-define('ITEMS_PER_PAGE',10);
-define('DISPLAY_PAGES',10);
-define('FRIENDLY_URL', true);
-define('COUNT_POSTS', true);
-define('HTML_EDITOR', true);
-define('DATE_FORMAT', 'dd-mm-yyyy');
-define('MIN_SEARCH_CHAR',4);
-define('PASSWORD_SIZE',8);
-define('SEPARATOR',' | ');
-define('SIDEBAR_ORIG','account,item_tools,new,search,locations,categories_cloud,infolinks,comments,advertisement,donate,popular,links,theme,translator,rss');
-define('SIDEBAR','new,account,item_tools,search,locations,infolinks,advertisement,donate,popular');
-define('ADVERT_TOP','<script type=\"text/javascript\">google_ad_client = \"pub-9818256176049741\";google_ad_slot = \"5864321500\"; google_ad_width = 728;google_ad_height = 15;</script><script type=\"text/javascript\" src=\"http://pagead2.googlesyndication.com/pagead/show_ads.js\"></script>');
-define('ADVERT_SIDEBAR','<script type=\"text/javascript\">google_ad_client = \"pub-9818256176049741\";google_ad_slot = \"4162447127\";google_ad_width = 250;google_ad_height = 250;</script><script type=\"text/javascript\" src=\"http://pagead2.googlesyndication.com/pagead/show_ads.js\"></script>');
-define('CURRENCY','&dollar;');
-define('CURRENCY_FORMAT','AMOUNTCURRENCY');
-define('MAX_IMG_SIZE', 4194304);
-define('IMG_UPLOAD', '/images/');
-define('IMG_UPLOAD_DIR', SITE_ROOT.IMG_UPLOAD);
-define('MAX_IMG_NUM',4);
-define('IMG_TYPES','gif,jpeg,jpg,png');
-define('IMG_RESIZE',900);
-define('IMG_RESIZE_THUMB',100);
-define('RSS_ITEMS',15);
-define('RSS_IMAGES',true);
-define('SITEMAP_FILE',SITE_ROOT.'/sitemap.xml.gz');
-define('SITEMAP_EXPIRE',86400);
-define('SITEMAP_DEL_ON_POST',true);
-define('SITEMAP_DEL_ON_CAT',true);
-define('SITEMAP_PING',false);
-define('TYPE_OFFER',0);
-define('TYPE_NEED',1);
-define('CACHE_ACTIVE',true);
-define('CACHE_TYPE','fileCache');
-define('CACHE_DATA_FILE',SITE_ROOT.'/cache/');
-define('CACHE_EXPIRE',86400);
-define('CACHE_DEL_ON_POST',true);
-define('CACHE_DEL_ON_CAT',true);
-define('ANALYTICS','');
-define('AKISMET','');
-define('MAP_KEY','');
-define('MAP_INI_POINT','');
-define('DISQUS','');
-define('GMAIL',false);
-define('GMAIL_USER','');
-define('GMAIL_PASS','');
-define('VIDEO',false);
-define('LOCATION',false);
-define('LOCATION_ROOT','');
-define('SMTP_HOST','');
-define('SMTP_PORT','');
-define('SMTP_AUTH',false);
-define('SMTP_USER','');
-define('SMTP_PASS','');
-define('LOGON_TO_POST',false);
-define('OCAKU_KEY','".$apiKey."');
-define('ALLOWED_HTML_TAGS','<b><i><u><div><center><blockquote><li><ul><a><p><br><br />');
-define('SITE_DESCRIPTION','');
-define('PARENT_POSTS',true);
-define('CONFIRM_POST',false);
-define('MODERATE_POST',false);
-define('EXPIRE_POST',0);
-define('RSS_SIDEBAR_URL','http://open-classifieds.com/feed');
-define('RSS_SIDEBAR_NAME','Open Classifieds');
-define('RSS_SIDEBAR_COUNT','5');
-define('PAYPAL_ACTIVE',false);
-define('PAYPAL_ACCOUNT','paypal@open-classifieds.com');
-define('PAYPAL_AMOUNT','2');
-define('PAYPAL_CURRENCY','USD');
-define('PAYPAL_SANDBOX',false);
-define('PAYPAL_AMOUNT_CATEGORY',false);
-define('CAPTCHA',true);
-define('SPAM_COUNTRY',false);
-define('SPAM_COUNTRIES','');
-define('NEED_OFFER',false);
-\n?>";//	echo $config_content;
-	
-		if (!oc::fwrite('../includes/config.php', $config_content))
-		{
-			$error_msg=T_("The configuration file")." '/includes/config.php' ".T_("is not writable").". ".T_("Change its permissions, then try again").".";
-			$install=false;
-		}
-	}
 
 ///////////////////////////////////////////////////////
 	//create robots.txt
@@ -173,7 +78,7 @@ define('NEED_OFFER',false);
 	{
 		if (!regenerateRobots($_POST["SITE_URL"]))
 		{
-			$error_msg = T_("Cannot write to the configuration file")." '/robots.txt'";
+			$error_msg = __("Cannot write to the configuration file")." '/robots.txt'";
 			$install=false;
 		}
 	}
@@ -184,14 +89,14 @@ define('NEED_OFFER',false);
 	{		
 		if (!regenerateHtaccess($_POST["SITE_URL"]))
 		{
-			$error_msg = T_("Cannot write to the configuration file")." '/.htaccess'";
+			$error_msg = __("Cannot write to the configuration file")." '/.htaccess'";
 			$install=false;
 		}
 	}
 	
 ///////////////////////////////////////////////////////
 	//all good!
-	if ($install) unlink("../install.lock");//prevents from performing a new install
+	if ($install) unlink('install/install.lock');//prevents from performing a new install
 	
 	//not succesful installation, let them know what was wrong
 	if (!$install && !empty($error_msg)) {
@@ -200,23 +105,22 @@ define('NEED_OFFER',false);
 	else{
 		//let them know installation suceed
 		?>
-		<div class="alert alert-success"><?php echo T_("Congratulations").". ".T_("Installation done");?></div>
+		<div class="alert alert-success"><?=__("Congratulations");?></div>
 		<div class="hero-unit">
-			<h1><?php _e('Installation done');?></h1>
+			<h1><?=__('Installation done');?></h1>
 			<p>
-				<?php echo _e("Please now erase the folder");?> <code>/install/</code><br>
+				<?=__("Please now erase the folder");?> <code>/install/</code><br>
 			
-				<a class="btn btn-success btn-large" href="<?php echo $_POST['SITE_URL'];?>"><?php _e("Go to Your Website");?></a>
+				<a class="btn btn-success btn-large" href="<?=$_POST['SITE_URL'];?>"><?=__("Go to Your Website")?></a>
 				
-				<a class="btn btn-warning btn-large" href="<?php echo $_POST['SITE_URL'];?>/admin"">Admin</a> 
-				<span class="help-block">user: <?php echo $_POST["ADMIN"];?> pass: <?php echo $_POST["ADMIN_PWD"];?></span>
+				<a class="btn btn-warning btn-large" href="<?=$_POST['SITE_URL'];?>/admin"">Admin</a> 
+				<span class="help-block">user: <?=$_POST["ADMIN"]?> pass: <?=$_POST["ADMIN_PWD"]?></span>
 				<hr>
-				<a class="btn btn-primary btn-large" href="http://j.mp/ocdonate"><?php _e("Make a donation");?></a>
-				<?php _e("We really appreciate it");?>.
+				<a class="btn btn-primary btn-large" href="http://j.mp/ocdonate"><?=__("Make a donation")?></a>
+				<?=__("We really appreciate it")?>.
 			</p>
 		</div>
-		<?php
-		include 'footer.php'; die();
+		<?
 	}
 	
 }
