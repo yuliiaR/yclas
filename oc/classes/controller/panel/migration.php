e338998fe520eb05b0e30e28201ec1e7f821cdde
Chema <neo22s@gmaill.com>
2013-03-19 14:04:09 +0100

    migration

--- oc/classes/controller/panel/migration.php
+++ oc/classes/controller/panel/migration.php
@@ -101,6 +101,7 @@ class Controller_Panel_Migration extends Auth_Controller {
 
     private function migrate($db,$pf)
     {
+        
         $accounts = $db->query(Database::SELECT, 'SELECT * FROM `'.$pf.'accounts`');
 
         //oc_accounts --> oc_users
@@ -119,6 +120,50 @@ class Controller_Panel_Migration extends Auth_Controller {
             $user->save();
         }
 
+        //categories --> categories
+        $categories_map = array(0=>1);
+
+        $categories = $db->query(Database::SELECT, 'SELECT * FROM `'.$pf.'categories` ORDER BY `idCategoryParent` ASC');
+
+        foreach ($categories as $category) 
+        {
+            $cat = new Model_Category();
+            $cat->name      = $category['name'];
+            $cat->order     = $category['order'];
+            $cat->created   = $category['created'];
+            $cat->seoname   = $category['friendlyName'];
+            $cat->price     = $category['price'];
+            $cat->description = $category['description'];
+            $cat->parent_deep = ($category['idCategoryParent']>0)? 1:0; //there's only 1 deep
+            $cat->id_category_parent = (isset($categories_map[$category['idCategoryParent']]))?$categories_map[$category['idCategoryParent']]:1;
+            $cat->save();
+
+            //we save old_id stores the new ID, so later we know the category parent, and to changes the ADS category id
+            $categories_map[$category['idCategory']] = $cat->id_category;
+
+        }
+
+
+        //locations --> locations
+        $locations_map = array(0=>1);
+
+        $locations = $db->query(Database::SELECT, 'SELECT * FROM `'.$pf.'locations` ORDER BY `idLocationParent` ASC');
+
+        foreach ($locations as $location) 
+        {
+            $loc = new Model_Location();
+            $loc->name      = $location['name'];
+            $loc->seoname   = $location['friendlyName'];
+            $loc->parent_deep = ($location['idLocationParent']>0)? 1:0; //there's only 1 deep
+            $loc->id_location_parent = (isset($locations_map[$location['idLocationParent']]))?$locations_map[$location['idLocationParent']]:1;
+            $loc->save();
+
+            //we save old_id stores the new ID, so later we know the location parent, and to changes the ADS location id
+            $locations_map[$location['idLocation']] = $loc->id_location;
+
+        }
+
+        
         die();
     }
 
