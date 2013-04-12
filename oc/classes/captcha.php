03f915bac9bd569cfbbbb9d92cf839bd69524004
Chema <neo22s@gmaill.com>
2013-04-12 11:18:53 +0200

    closes #79

--- oc/classes/captcha.php
+++ oc/classes/captcha.php
@@ -1,34 +1,36 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
 /**
-* ultra light captcha class
-*
-* @package Open Classifieds
-* @subpackage Core
-* @category Helper
-* @author Chema Garrido <chema@garridodiaz.com>
-* @license GPL v3
-*/
+ * ultra light captcha class
+ *
+ * @package Open Classifieds
+ * @subpackage Core
+ * @category Helper
+ * @author Chema Garrido <chema@garridodiaz.com>
+ * @license GPL v3
+ */
 
 class captcha{
 
     /**
-	* generates the image for the captcha
-	* @param string $name, used in the session
-	* @param int $width
-	* @param int $height
-	* @param string $baseList
-	*/
+	 * generates the image for the captcha
+	 * @param string $name, used in the session
+	 * @param int $width
+	 * @param int $height
+	 * @param string $baseList
+	 */
     public static function image($name='',$width=120,$height=40,$baseList = '0123456789abcdfghjkmnpqrstvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
     {
+        //echo $name.' orig= '.Session::instance()->get('captcha_'.$name).'<br>';
 
         $length = mt_rand(3,5);//code length
-        $lines = mt_rand(1,5);//to make the image dirty
-        $image = @imagecreate($width, $height) or die('Cannot initialize GD!');
-        $code = ''; //code generated saved at session
+        $lines  = mt_rand(1,5);//to make the image dirty
+        $image  = @imagecreate($width, $height) or die('Cannot initialize GD!');
+        $code   = ''; //code generated saved at session
 
         //base image with dirty lines
-        for( $i=0; $i<$lines; $i++ ) {
+        for( $i=0; $i<$lines; $i++ ) 
+        {
            imageline($image,
                  mt_rand(0,$width), mt_rand(0,$height),
                  mt_rand(0,$width), mt_rand(0,$height),
@@ -36,7 +38,8 @@ class captcha{
         }
 
         //writting the chars
-        for( $i=0, $x=0; $i<$length; $i++ ) {
+        for( $i=0, $x=0; $i<$length; $i++ ) 
+        {
            $actChar = substr($baseList, rand(0, strlen($baseList)-1), 1);
            $x += 10 + mt_rand(0,10);
            imagechar($image, mt_rand(4,5), $x, mt_rand(5,20), $actChar,
@@ -44,7 +47,9 @@ class captcha{
            $code .= strtolower($actChar);
         }
         
+        Session::instance()->set('captcha_'.$name, $code);   
 
+        //die( 'changed= '.Session::instance()->get('captcha_'.$name));
         
         // prevent client side caching
         header("Expires: Wed, 1 Jan 1997 00:00:00 GMT");
@@ -54,45 +59,44 @@ class captcha{
         header("Pragma: no-cache");
         header('Content-Type: image/jpeg');
         imagejpeg($image);
-        imagedestroy($image);
-        
-        Session::instance()->set('captcha'.$name, $code);   
+        imagedestroy($image);    
         
     }
 
     
     /**
-	*
-	* @param string $name for the session
-	*/
+	 *
+	 * @param string $name for the session
+	 */
     public static function url($name='')
     {
         return Route::url('default',array('controller'  => 'captcha', 'action' => 'image', 'id'=>$name)); 
     }
     
     /**
-	* check if its valid or not
-	* @param string $name for the session
-	* @return boolean
-	*/
-    public static function check($name='')
-    { //TODO From config
-        // if (CAPTCHA)
-        // {
-            if (Session::instance()->get('captcha'.$name)==strtolower($_POST['captcha'])) //TODO use core 
-            {
-                Session::instance()->set('captcha'.$name, '');
-                return TRUE;
-            }
-            else return FALSE;
-        // }
-        // else return TRUE;
+	 * check if its valid or not
+	 * @param string $name for the session
+	 * @return boolean
+	 */
+    public static function check($name = '')
+    { 
+        //d(strtolower(core::post('captcha')));
+        //d(Session::instance()->get('captcha_'.$name));
+        //d(Session::instance()->get('captcha_'.$name) == strtolower(core::post('captcha')));
+
+        if (Session::instance()->get('captcha_'.$name) == strtolower(core::post('captcha'))) 
+        {
+            Session::instance()->set('captcha_'.$name, '');
+            return TRUE;
+        }
+        else return FALSE;
+        
     }
     
     /**
-	* javascript that allow us to reload the iamge in case you can't read it
-	* @return string javascript
-	*/
+	 * javascript that allow us to reload the iamge in case you can't read it
+	 * @return string javascript
+	 */
     public static function reload_image()
     {
         return '<script type="text/javascript">
@@ -106,10 +110,10 @@ class captcha{
     }
 
     /**
-	* generates the HTML image tag to add in a form
-	* @param string $name unique name for the image
-	* @return string html tag
-	*/
+	 * generates the HTML image tag to add in a form
+	 * @param string $name unique name for the image
+	 * @return string html tag
+	 */
     public static function image_tag($name='')
     {
         return self::reload_image().
--- oc/classes/controller/captcha.php
+++ oc/classes/controller/captcha.php
@@ -5,6 +5,11 @@ class Controller_Captcha extends Kohana_Controller {
 	public function action_image()
 	{
 		$token = $this->request->param('id');
+
+        //removig the & we add to refresh the image.
+        if (($amp_pos = strpos($token, '&'))>1)
+            $token = substr($token, 0,$amp_pos);
+        
 		$captcha = new captcha();
 		die($captcha->image($token));
 	}
