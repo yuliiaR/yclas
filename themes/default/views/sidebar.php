9c74d27a049ea500b3031de290113b325f285757
Chema <neo22s@gmaill.com>
2013-06-05 13:10:38 +0200

    closes #248

--- themes/default/views/sidebar.php
+++ themes/default/views/sidebar.php
@@ -3,15 +3,17 @@
     <div class="span3">
         <div class="category_box_title custom_box">
         </div>
-        <div class="well custom_box_content" style="padding: 8px 0;">
+        <div class="well custom_box_content" >
             <?=$widget->render()?>
         </div>
     </div>
 <?endforeach?>
 <?if (Theme::get('premium')!=1):?>
 <div class="span3">
+    <div class="custom_box_content" >
     <script type="text/javascript">if (typeof geoip_city!="function")document.write("<scr"+"ipt type=\"text/javascript\" src=\"http://j.maxmind.com/app/geoip.js\"></scr"+"ipt>");
         document.write("<scr"+"ipt type=\"text/javascript\" src=\"http://api.adserum.com/sync.js?a=6&f=3&w=200&h=200\"></scr"+"ipt>");
     </script>
+    </div>
 </div>
 <?endif?>
