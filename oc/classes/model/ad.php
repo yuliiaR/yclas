6384741c377534a3c0356b9a5a9974434b2a089d
Slobodan <slobodan.josifovic@gmail.com>
2013-06-07 09:15:12 +0200

    removed one coment

--- oc/classes/model/ad.php
+++ oc/classes/model/ad.php
@@ -298,7 +298,6 @@ class Model_Ad extends ORM {
                 $root           = core::config('general.base_url');
                 $path           = $this->image_path($id , $created);
                 $directory      = DOCROOT.$path;
-                // d(DOCROOT."--app ".APPPATH."--sys ".SYSPATH."--".$root);
                 $image_quality  = core::config('image.quality');
                 $width          = core::config('image.width');
                 $width_thumb    = core::config('image.width_thumb');
