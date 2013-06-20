81c1f10974ae33bfd6038a48f77c77db01a2b8fe
Chema <neo22s@gmaill.com>
2013-06-20 16:05:37 +0200

    fixes on ocean theme probelm with sidebar

--- themes/default/views/sidebar.php
+++ themes/default/views/sidebar.php
@@ -1,19 +1,19 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
+<div class="span3"> 
 <?foreach ( widgets::get('sidebar') as $widget):?>
-    <div class="span3">
+    
         <div class="category_box_title custom_box">
         </div>
         <div class="well custom_box_content" >
             <?=$widget->render()?>
         </div>
-    </div>
+   
 <?endforeach?>
 <?if (Theme::get('premium')!=1):?>
-<div class="span3">
     <div class="custom_box_content" >
     <script type="text/javascript">if (typeof geoip_city!="function")document.write("<scr"+"ipt type=\"text/javascript\" src=\"http://j.maxmind.com/app/geoip.js\"></scr"+"ipt>");
         document.write("<scr"+"ipt type=\"text/javascript\" src=\"http://api.adserum.com/sync.js?a=6&f=3&w=200&h=200\"></scr"+"ipt>");
     </script>
     </div>
-</div>
 <?endif?>
+</div>
