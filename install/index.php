b105027fdcf65254f72bc6772206c08dbb76defa
Chema <neo22s@gmaill.com>
2013-07-08 14:18:56 +0200

    bootstrap for isntallation

--- install/index.php
+++ install/index.php
@@ -47,7 +47,7 @@ include 'install.php';
       }
     </style>
     	
-	<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+	<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
 
   </head>
 
