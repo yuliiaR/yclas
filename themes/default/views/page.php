8cd426c1f91f5f2ef09840ac902763f6eb363cae
Chema <neo22s@gmaill.com>
2013-07-13 08:58:24 +0200

    closes #312

--- themes/default/views/page.php
+++ themes/default/views/page.php
@@ -1,9 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
 
 <div class="page-header">
-    <h1><?echo $page->title?></h1>
+    <h1><?=$page->title?></h1>
 </div>
 
 <div class="well">
-	<?=$page->description?>
+	<?=Text::bb2html($page->description,TRUE)?>
 </div><!-- /well -->
