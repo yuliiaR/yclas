0bce7d25a13e99984c8a16f740e408efaca22fa9
Slobodan <slobodan.josifovic@gmail.com>
2013-07-26 09:44:42 +0200

    replaced short tags close #319

--- install/index.php
+++ install/index.php
@@ -1,4 +1,4 @@
-<? 
+<?php  
 /**
  * HTML for the install
  *
@@ -22,10 +22,10 @@ include 'install.php';
 	<meta charset="utf8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title>Open Classifieds <?=__("Installation")?></title>
+	<title>Open Classifieds <?php echo __("Installation")?></title>
     <meta name="keywords" content="" >
     <meta name="description" content="" >
-    <meta name="copyright" content="Open Classifieds <?=VERSION?>" >
+    <meta name="copyright" content="Open Classifieds <?php echo VERSION?>" >
 	<meta name="author" content="Open Classifieds">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 
@@ -56,7 +56,7 @@ include 'install.php';
     <!--phpinfo Modal -->
     <div id="phpinfoModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-body">
-        <?
+        <?php 
         //getting the php info clean!
         ob_start();                                                                                                        
         phpinfo();                                                                                                     
@@ -74,7 +74,7 @@ include 'install.php';
 
 	<div class="navbar navbar-fixed-top navbar-inverse">
 	<div class="navbar-inner">
-	<div class="container"><a class="brand">Open Classifieds <?=__("Installation")?></a>
+	<div class="container"><a class="brand">Open Classifieds <?php echo __("Installation")?></a>
 	<div class="nav-collapse">
 
 	<div class="btn-group pull-right">
@@ -94,10 +94,10 @@ include 'install.php';
             	<div class="well sidebar-nav">
             	
             		<ul class="nav nav-list">
-            			<li class="nav-header"><?=__("Requirements")?></li>
+            			<li class="nav-header"><?php echo __("Requirements")?></li>
             			<li class="divider"></li>
             			
-            			<?foreach ($checks as $name => $values):
+            			<?php foreach ($checks as $name => $values):
             				if ($values['mandatory'] == TRUE AND $values['result'] == FALSE)
             					$succeed = FALSE;
 
@@ -105,10 +105,10 @@ include 'install.php';
             					$msg .= $values['message'].'<br>';
             			?>
 
-            				<li><i class="icon-<?=($values['result'])?"ok":"remove"?>"></i> 
-            					<?=$name?>
+            				<li><i class="icon-<?php echo ($values['result'])?"ok":"remove"?>"></i> 
+            					<?php echo $name?>
             				</li>
-            			<?endforeach?>
+            			<?php endforeach?>
 
             			<li class="divider"></li>
             			<li><a href="#phpinfoModal" role="button" data-toggle="modal">PHP Info</a></li>
@@ -137,54 +137,54 @@ include 'install.php';
             <!--/span-->	
 
 <div class="span9">
-<?if ($_POST && $succeed):?>
+<?php if ($_POST && $succeed):?>
 
-	<?if (!$install && !empty($error_msg)):?>
-		 <div class="alert alert-error"><?=$error_msg?></div>
-		<?hostingAd()?>
-	<?elseif($install==TRUE):?>
-		<div class="alert alert-success"><?=__('Congratulations');?></div>
+	<?php if (!$install && !empty($error_msg)):?>
+		 <div class="alert alert-error"><?php echo $error_msg?></div>
+		<?php hostingAd()?>
+	<?php elseif($install==TRUE):?>
+		<div class="alert alert-success"><?php echo __('Congratulations');?></div>
 		<div class="hero-unit">
-			<h1><?=__('Installation done');?></h1>
+			<h1><?php echo __('Installation done');?></h1>
 			<p>
-				<?=__('Please now erase the folder');?> <code>/install/</code><br>
+				<?php echo __('Please now erase the folder');?> <code>/install/</code><br>
 			
-				<a class="btn btn-success btn-large" href="<?=$_POST['SITE_URL']?>"><?=__('Go to Your Website')?></a>
+				<a class="btn btn-success btn-large" href="<?php echo $_POST['SITE_URL']?>"><?php echo __('Go to Your Website')?></a>
 				
-				<a class="btn btn-warning btn-large" href="<?=$_POST['SITE_URL']?>oc-panel/home/">Admin</a> 
-				<?if($_POST['ADMIN_EMAIL'])?><span class="help-block">user: <?=$_POST['ADMIN_EMAIL']?> pass: <?=$_POST['ADMIN_PWD']?></span>
+				<a class="btn btn-warning btn-large" href="<?php echo $_POST['SITE_URL']?>oc-panel/home/">Admin</a> 
+				<?php if($_POST['ADMIN_EMAIL'])?><span class="help-block">user: <?php echo $_POST['ADMIN_EMAIL']?> pass: <?php echo $_POST['ADMIN_PWD']?></span>
 				<hr>
-				<a class="btn btn-primary btn-large" href="http://j.mp/thanksdonate"><?=__('Make a donation')?></a>
-				<?=__('We really appreciate it')?>.
+				<a class="btn btn-primary btn-large" href="http://j.mp/thanksdonate"><?php echo __('Make a donation')?></a>
+				<?php echo __('We really appreciate it')?>.
 			</p>
 		</div>
-	<?endif?>
+	<?php endif?>
 
-<?elseif ($succeed):?>
+<?php elseif ($succeed):?>
 
 <div class="page-header">
-	<h1><?=__("Welcome to")?> Open Classifieds <?=__("installation")?></h1>
+	<h1><?php echo __("Welcome to")?> Open Classifieds <?php echo __("installation")?></h1>
 	<p>
-		<?=__("Welcome to the super easy and fast installation")?>. 
+		<?php echo __("Welcome to the super easy and fast installation")?>. 
 			<a href="http://open-classifieds.com/market/" target="_blank">
-			<?=__("If you need any help please check our professional services")?></a>.
+			<?php echo __("If you need any help please check our professional services")?></a>.
 	</p>	
 </div>
 
-<?if ($msg){?>
-	<div class="alert alert-warning"><?=$msg?></div>
-<?hostingAd();}?>
+<?php if ($msg){?>
+	<div class="alert alert-warning"><?php echo $msg?></div>
+<?php hostingAd();}?>
 
 <form method="post" action="" class="well" >
 <fieldset>
 
-<h2><?=__('Site Configuration')?></h2>
+<h2><?php echo __('Site Configuration')?></h2>
 
 <div class="control-group">
-	<label class="control-label"><?=__("Site Language")?></label>
+	<label class="control-label"><?php echo __("Site Language")?></label>
 	<div class="controls">
        <select name="LANGUAGE" onchange="window.location.href='?LANGUAGE='+this.options[this.selectedIndex].value">
-		    <?
+		    <?php 
 		    $languages = scandir("languages");
 		    foreach ($languages as $lang) {
 			    
@@ -200,62 +200,62 @@ include 'install.php';
 </div>
 
 <div class="control-group">
-	<label class="control-label"><?=__("Site URL");?>:</label>
+	<label class="control-label"><?php echo __("Site URL");?>:</label>
 	<div class="controls">
-    <input  type="text" size="75" name="SITE_URL" value="<?=cP('SITE_NAME',$suggest_url)?>"  class="span6" />
+    <input  type="text" size="75" name="SITE_URL" value="<?php echo cP('SITE_NAME',$suggest_url)?>"  class="span6" />
 	</div>
 </div>
 
 <div class="control-group">
-	<label class="control-label"><?=__("Site Name")?>:</label>
+	<label class="control-label"><?php echo __("Site Name")?>:</label>
 	<div class="controls">
-	<input  type="text" name="SITE_NAME" placeholder="<?=__("Site Name")?>" value="<?=cP('SITE_NAME')?>" class="span6" />
+	<input  type="text" name="SITE_NAME" placeholder="<?php echo __("Site Name")?>" value="<?php echo cP('SITE_NAME')?>" class="span6" />
 	</div>
 </div>
 
 <div class="control-group">
-	<label class="control-label"><?=__("Time Zone")?>:</label>
+	<label class="control-label"><?php echo __("Time Zone")?>:</label>
 	<div class="controls">
-	<?=get_select_timezones('TIMEZONE',cP('TIMEZONE',date_default_timezone_get()))?>
+	<?php echo get_select_timezones('TIMEZONE',cP('TIMEZONE',date_default_timezone_get()))?>
 	</div>
 </div>
 
 
 <ul class="nav nav-tabs" id="myTab">
-  <li class="active"><a href="#install" data-toggle="tab"><?=__('New Install')?></a></li>
-  <li><a href="#upgrade" data-toggle="tab"><?=__('Upgrade System')?></a></li>
+  <li class="active"><a href="#install" data-toggle="tab"><?php echo __('New Install')?></a></li>
+  <li><a href="#upgrade" data-toggle="tab"><?php echo __('Upgrade System')?></a></li>
 </ul>
  
 <div class="tab-content">
 
     <div class="tab-pane active" id="install">
         <div class="control-group">
-            <label class="control-label"><?=__("Administrator email")?>:</label>
+            <label class="control-label"><?php echo __("Administrator email")?>:</label>
             <div class="controls">
-                <input type="text" name="ADMIN_EMAIL" value="<?=cP('ADMIN_EMAIL')?>" placeholder="your@email.com" class="span6" />
+                <input type="text" name="ADMIN_EMAIL" value="<?php echo cP('ADMIN_EMAIL')?>" placeholder="your@email.com" class="span6" />
             </div>
         </div>
 
         <div class="control-group">
-            <label class="control-label"><?=__("Admin Password")?>:</label>
+            <label class="control-label"><?php echo __("Admin Password")?>:</label>
             <div class="controls">
-                <input type="text" name="ADMIN_PWD" value="<?=cP('ADMIN_PWD')?>" class="span6" />   
+                <input type="text" name="ADMIN_PWD" value="<?php echo cP('ADMIN_PWD')?>" class="span6" />   
             </div>
         </div>
 
         <div class="control-group">
-            <label class="checkbox"><input type="checkbox" name="SAMPLE_DB" checked /><?=__("Sample data")?></label>
-            <span class="help-block"><?=__("Creates few sample categories to start with")?></span>
+            <label class="checkbox"><input type="checkbox" name="SAMPLE_DB" checked /><?php echo __("Sample data")?></label>
+            <span class="help-block"><?php echo __("Creates few sample categories to start with")?></span>
         </div>
         
     </div>
 
     <div class="tab-pane" id="upgrade">
         <div class="control-group">
-            <label class="control-label"><?=__("Hash Key")?>:</label>
+            <label class="control-label"><?php echo __("Hash Key")?>:</label>
             <div class="controls">
-                <input type="text" name="HASH_KEY" value="<?=cP('HASH_KEY')?>" class="span6" />   
-                <span class="help-block"><?=__('You need the Hash Key to re-install. You can find this value if you lost it at')?> <code>/oc/config/auth.php</code></span>
+                <input type="text" name="HASH_KEY" value="<?php echo cP('HASH_KEY')?>" class="span6" />   
+                <span class="help-block"><?php echo __('You need the Hash Key to re-install. You can find this value if you lost it at')?> <code>/oc/config/auth.php</code></span>
             </div>
         </div>
     </div>
@@ -263,61 +263,61 @@ include 'install.php';
 </div>
 
 
-<h2><?=__('Database Configuration')?></h2>
+<h2><?php echo __('Database Configuration')?></h2>
 
 <div class="control-group">
-	<label class="control-label"><?=__("Host name")?>:</label>
+	<label class="control-label"><?php echo __("Host name")?>:</label>
 	<div class="controls">
-	<input  type="text" name="DB_HOST" value="<?=cP('DB_HOST','localhost')?>" class="span6"  />
+	<input  type="text" name="DB_HOST" value="<?php echo cP('DB_HOST','localhost')?>" class="span6"  />
 	</div>
 </div>
 
 <div class="control-group">
-	<label class="control-label"><?=__("User name")?>:</label>
+	<label class="control-label"><?php echo __("User name")?>:</label>
 	<div class="controls">
-	<input  type="text" name="DB_USER"  value="<?=cP('DB_USER','root')?>" class="span6"   />
+	<input  type="text" name="DB_USER"  value="<?php echo cP('DB_USER','root')?>" class="span6"   />
 	</div>
 </div>
 
 <div class="control-group">
-	<label class="control-label"><?=__("Password")?>:</label>
+	<label class="control-label"><?php echo __("Password")?>:</label>
 	<div class="controls">
-	<input type="text" name="DB_PASS" value="<?=cP('DB_PASS')?>" class="span6" />		
+	<input type="text" name="DB_PASS" value="<?php echo cP('DB_PASS')?>" class="span6" />		
 	</div>
 </div>
 
 <div class="control-group">
-	<label class="control-label"><?=__("Database name")?>:</label>
+	<label class="control-label"><?php echo __("Database name")?>:</label>
 	<div class="controls">
-	<input type="text" name="DB_NAME" value="<?=cP('DB_NAME','openclassifieds')?>"  class="span6"  />
+	<input type="text" name="DB_NAME" value="<?php echo cP('DB_NAME','openclassifieds')?>"  class="span6"  />
 	</div>
 </div>
 
 <div class="control-group">
-	<label class="control-label"><?=__("Database charset")?>:</label>
+	<label class="control-label"><?php echo __("Database charset")?>:</label>
 	<div class="controls">
-	<input type="text" name="DB_CHARSET" value="<?=cP('DB_CHARSET','utf8')?>"  class="span6"   />
+	<input type="text" name="DB_CHARSET" value="<?php echo cP('DB_CHARSET','utf8')?>"  class="span6"   />
 	</div>
 </div>
 
 <div class="control-group">
-	<label class="control-label"><?=__("Table prefix")?>:</label>
+	<label class="control-label"><?php echo __("Table prefix")?>:</label>
 	<div class="controls">
-	<input type="text" name="TABLE_PREFIX" value="<?=cP('TABLE_PREFIX','oc2_')?>" class="text-medium" />
-	<span class="help-block"><?=__("Allows multiple installations in one database if you give each one a unique prefix")?>. <?=__("Only numbers, letters, and underscores")?>.</span>
+	<input type="text" name="TABLE_PREFIX" value="<?php echo cP('TABLE_PREFIX','oc2_')?>" class="text-medium" />
+	<span class="help-block"><?php echo __("Allows multiple installations in one database if you give each one a unique prefix")?>. <?php echo __("Only numbers, letters, and underscores")?>.</span>
 	</div>
 </div>
 
 
 <div class="form-actions">
 
-	<input type="submit" name="action" id="submit" value="<?=__("Install")?>" class="btn btn-primary btn-large" />
+	<input type="submit" name="action" id="submit" value="<?php echo __("Install")?>" class="btn btn-primary btn-large" />
     <hr>
     <div class="control-group">
         <label class="checkbox">
             <input type="checkbox" name="OCAKU" checked />
-            <?=__("Ocacu classifieds community registration")?> <a target="_blank" href="http://ocacu.com/en/terms.html">
-                <?=__('Terms')?></a>
+            <?php echo __("Ocacu classifieds community registration")?> <a target="_blank" href="http://ocacu.com/en/terms.html">
+                <?php echo __('Terms')?></a>
         </label>
     </div>
 </div>
@@ -325,12 +325,12 @@ include 'install.php';
 </fieldset>
 </form>
 
-<?else:?>
+<?php else:?>
 
-	<div class="alert alert-error"><?=$msg?></div>
-	<?hostingAd()?>
+	<div class="alert alert-error"><?php echo $msg?></div>
+	<?php hostingAd()?>
 
-<?endif?>
+<?php endif?>
 
 </div><!--/span--> 
 </div><!--/row-->
@@ -338,7 +338,7 @@ include 'install.php';
 
 <footer>
 <p>
-&copy; 	<a href="http://open-classifieds.com" title="Open Source PHP Classifieds">Open Classifieds</a> 2009 - <?=date('Y')?>
+&copy; 	<a href="http://open-classifieds.com" title="Open Source PHP Classifieds">Open Classifieds</a> 2009 - <?php echo date('Y')?>
 </p>
 </footer>    
 
