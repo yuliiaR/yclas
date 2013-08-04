9569aebfe0b7eb9cb7d72c1302574b88ef8d6176
WinterSilence <info@handy-soft.ru>
2013-08-05 02:28:32 +0400

    full fix
    corrected all - the old version of painful to watch

--- oc/classes/view.php
+++ oc/classes/view.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 /**
  * Extended functionality for Kohana View
  *
@@ -8,7 +8,6 @@
  * @copyright  (c) 2009-2013 Open Classifieds Team
  * @license    GPL v3
  */
-
 class View extends Kohana_View{
     
 	// TODO: Delete this method and move his code into View::capture
@@ -42,7 +41,7 @@ class View extends Kohana_View{
      */
     public static function delete_fragment($name)
     {
-        return Core::cache(self::fragment_name($name),NULL,0);
+        return Core::cache(self::fragment_name($name), NULL, 0);
     }
 
     
@@ -51,24 +50,28 @@ class View extends Kohana_View{
      * @param  string $name 
      * @return string       
      */
-    public static function fragment_name($name)
-    {
-        $cat_seoname = '';
-        if (Controller::$category!==NULL)
-        {
-            if (Controller::$category->loaded())
-                $cat_seoname = '_category_'.Controller::$category->seoname;
-        }
+	public static function fragment_name($name)
+	{
+		if ( ! is_null(Controller::$category) AND Controller::$category->loaded())
+		{
+			$cat_seoname = '_category_'.Controller::$category->seoname;
+		}
+		else
+		{
+			$cat_seoname = '';
+		}
 
-        $loc_seoname = '';
-        if (Controller::$location!==NULL)
-        {
-            if (Controller::$location->loaded())
-                $loc_seoname = '_location_'.Controller::$location->seoname;
-        }
-
-        return 'fragment_'.$name.'_'.i18n::lang().'_'.Theme::$theme.$cat_seoname.$loc_seoname; //.Theme::$skin
-    }
+		if ( ! is_null(Controller::$location) AND Controller::$location->loaded())
+		{
+			$loc_seoname = '_location_'.Controller::$location->seoname;
+		}
+		else
+		{
+			$loc_seoname = '';
+		}
+		
+		return 'fragment_'.$name.'_'.i18n::lang().'_'.Theme::$theme.$cat_seoname.$loc_seoname; //.Theme::$skin
+	}
 
 
     /**
@@ -80,31 +83,19 @@ class View extends Kohana_View{
      * @return  View
      * @throws  View_Exception
      */
-    public function set_filename($file)
-    {
-    	//folder loaded as module in the bootstrap :D
-    	if (($path = Kohana::find_file(Theme::views_path(), $file)) === FALSE)
-    	{
-    		//in case view not found try to read from default theme
-    		if (($path = Kohana::find_file(Theme::default_views_path(), $file)) === FALSE)
-    		{
-	    		//still not found :(, try from cascading system
-	    		if (($path = Kohana::find_file('views', $file)) === FALSE)
-	    		{
-                    //d($file);
-	    			throw new View_Exception('The requested view :file could not be found', array(
-	    	                				':file' => $file,
-	    			));
-	    		}
-    		}
-    
-    	}
-    
-    	// Store the file path locally
-    	$this->_file = $path;
-    
-    	return $this;
-    }
+	public function set_filename($file)
+	{
+		// Folder loaded as module in the bootstrap :D
+		if ( ! $path = Kohana::find_file(Theme::views_path(), $file))
+		{
+			// In case view not found try to read from default theme
+			if ( ! $path = Kohana::find_file(Theme::default_views_path(), $file))
+			{
+				$path = $file;
+			}
+		}
+		// Still not found :(, try from cascading system
+		return parent::set_filename($path);
+	}
 
-    
 }
