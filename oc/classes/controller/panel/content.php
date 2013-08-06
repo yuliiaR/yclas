ad27384fa4652fc80ba5701e6e7b135446a05fef
Chema <neo22s@gmaill.com>
2013-08-06 10:19:43 +0200

    controller content locale

--- oc/classes/controller/panel/content.php
+++ oc/classes/controller/panel/content.php
@@ -5,7 +5,7 @@ class Controller_Panel_Content extends Auth_Crud {
 	/**
 	 * @var $_index_fields ORM fields shown in index
 	 */
-	protected $_index_fields = array('type','title','seotitle');
+	protected $_index_fields = array('type','title','seotitle','locale');
 
 	/**
 	 * @var $_orm_model ORM model name
