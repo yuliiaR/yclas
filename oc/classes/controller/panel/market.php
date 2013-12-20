0cee86d3ec56f62fc37c52125cf0db1ade0aa280
chema <neo22s@gmail.com>
2013-12-20 23:25:53 +0100

    modal market and themes

--- oc/classes/controller/panel/market.php
+++ oc/classes/controller/panel/market.php
@@ -13,8 +13,10 @@ class Controller_Panel_Market extends Auth_Controller {
         
         // validation active 
         Breadcrumbs::add(Breadcrumb::factory()->set_title(__('Market')));  
-        $this->template->title = __('Market');     
-        //$this->template->scripts['footer'][] = 'https://trychec.com/js/embed.js';
+        $this->template->title = __('Market');  
+
+        $market_url = (Kohana::$environment!== Kohana::DEVELOPMENT)? 'market.open-classifieds.com':'eshop.lo';
+        $this->template->scripts['footer'][] = 'http://'.$market_url.'/embed.js';
 
         $market = Core::get_market();
 
