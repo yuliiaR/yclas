bf49caa6ca20c67b482190f926c6e152916c02c3
chema <neo22s@gmail.com>
2014-01-03 11:54:26 +0100

    closes #484

--- oc/classes/model/user.php
+++ oc/classes/model/user.php
@@ -421,7 +421,7 @@ class Model_User extends ORM {
 
     public function exclude_fields()
     {
-       return array('last_modified','logins','last_login','created','salt', 'ip_created', 'last_ip','token','token_created','token_expires','user_agent','id_location');
+       return array('hybridauth_provider_uid','password','last_modified','created','salt', 'ip_created', 'last_ip','token','token_created','token_expires','user_agent','id_location');
     }
 
     /**
