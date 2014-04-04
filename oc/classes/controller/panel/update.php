35d1319476d29c3653f017f21aa718f88d11f246
Slobodan <slobodan.josifovic@gmail.com>
2014-04-04 10:33:57 +0200

    working on #472

--- oc/classes/controller/panel/update.php
+++ oc/classes/controller/panel/update.php
@@ -335,7 +335,8 @@ class Controller_Panel_Update extends Auth_Controller {
         {
             DB::query(Database::UPDATE,"INSERT INTO  `".$prefix."access` (`id_role`, `access`) VALUES 
                                                                          (7, 'profile.*'),(7, 'content.*'),(7, 'stats.user.*'),
-                                                                         (7, 'blog.*'),(7, 'translations.*'),(7, 'ad.*')")->execute();
+                                                                         (7, 'blog.*'),(7, 'translations.*'),(7, 'ad.*'),
+                                                                         (7, 'widgets.*'),(7, 'menu.*')")->execute();
         }catch (exception $e) {}
 
         //call update previous versions
