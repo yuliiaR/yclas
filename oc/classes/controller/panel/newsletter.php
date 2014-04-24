991a19ae30ea75d7a616dc988d572f30603cf673
chema <neo22s@gmail.com>
2014-04-24 16:11:33 +0200

    closes #626

--- oc/classes/controller/panel/newsletter.php
+++ oc/classes/controller/panel/newsletter.php
@@ -8,48 +8,188 @@
 class Controller_Panel_Newsletter extends Auth_Controller {
 
 
-
     public function action_index()
     {
-
-        // validation active 
         Breadcrumbs::add(Breadcrumb::factory()->set_title(__('Newsletter')));  
         $this->template->title = __('Newsletter');
-        //$this->template->styles = array('http://cdn.jsdelivr.net/sceditor/1.4.3/themes/default.min.css' => 'screen');
-        $this->template->scripts['footer'][] = 'js/jquery.sceditor.min.js';
-        
+
+        //count all users
         $user = new Model_User();
         $user->where('status','=',Model_User::STATUS_ACTIVE)->where('subscriber','=',1);
-        $user = $user->count_all();
+        $count_all_users = $user->count_all();
 
-        if($this->request->post())
-        {
-            $query = DB::select('email')->select('name')
-                        ->from('users')
-                        ->where('status','=',Model_User::STATUS_ACTIVE)
+        //count featured expired
+        $query = DB::select(DB::expr('COUNT(id_user) count'))
+                        ->from('ads')
+                        ->where('status','=',Model_Ad::STATUS_PUBLISHED)
+                        ->where('featured','<',DB::expr('NOW()'))
+                        ->group_by('id_user')
                         ->execute();
 
-            $users = $query->as_array();
-            if (count($users)>0 OR Core::post('subject')!=NULL)
+        $count_featured_expired = $query->as_array();
+        $count_featured_expired = (isset($count_featured_expired[0]['count']))?$count_featured_expired[0]['count']:0;
+
+        
+        //count all featured 
+        $query = DB::select(DB::expr('COUNT(id_user) count'))
+                        ->from('ads')
+                        ->where('status','=',Model_Ad::STATUS_PUBLISHED)
+                        ->where('featured','IS NOT',NULL)
+                        ->group_by('id_user')
+                        ->execute();
+
+        $count_featured = $query->as_array();
+        $count_featured = (isset($count_featured[0]['count']))?$count_featured[0]['count']:0;
+
+        //users without published ads
+        $query = DB::select(DB::expr('COUNT(id_user) count'))
+                        ->from(array('users','u'))
+                        ->join(array('ads','a'),'LEFT')
+                        ->using('id_user')
+                        ->where('u.status','=',Model_User::STATUS_ACTIVE)
+                        ->where('u.subscriber','=',1)
+                        ->where('a.title','is',NULL)
+                        ->group_by('u.id_user')
+                        ->execute();
+        $count_unpub = $query->as_array();
+        $count_unpub = (isset($count_unpub[0]['count']))?$count_unpub[0]['count']:0;
+
+        //count all users not login 3 months 
+        $query = DB::select(DB::expr('COUNT(id_user) count'))
+                        ->from('users')
+                        ->where('status','=',Model_User::STATUS_ACTIVE)
+                        ->where('last_login','<=',Date::unix2mysql(strtotime('-3 month')))
+                        ->or_where('last_login','IS',NULL)
+                        ->where('subscriber','=',1)
+                        ->execute();
+
+        $count_logged = $query->as_array();
+        $count_logged = (isset($count_logged[0]['count']))?$count_logged[0]['count']:0;
+
+        //count all users spammers
+        $query = DB::select(DB::expr('COUNT(id_user) count'))
+                        ->from('users')
+                        ->where('status','=',Model_User::STATUS_SPAM)
+                        ->where('subscriber','=',1)
+                        ->execute();
+
+        $count_spam = $query->as_array();
+        $count_spam = (isset($count_spam[0]['count']))?$count_spam[0]['count']:0;
+        
+
+        //post done sending newsletter
+        if($this->request->post() AND Core::post('subject')!=NULL)
+        {
+            $users = array();
+
+            if (core::post('send_all')=='on')
+            {
+                $query = DB::select('email')->select('name')
+                        ->from('users')
+                        ->where('status','=',Model_User::STATUS_ACTIVE)
+                        ->where('subscriber','=',1)
+                        ->execute();
+
+                $users = array_merge($users,$query->as_array());
+            }
+            
+            if (Theme::get('premium')==1)
+            {
+                if (core::post('send_featured_expired')=='on')
+                {
+                    $query = DB::select('email')->select('name')
+                            ->from(array('users','u'))
+                            ->join(array('ads','a'))
+                            ->using('id_user')
+                            ->where('a.status','=',Model_Ad::STATUS_PUBLISHED)
+                            ->where('a.featured','<',DB::expr('NOW()'))
+                            ->where('u.subscriber','=',1)
+                            ->group_by('id_user')
+                            ->execute();
+
+                    $users = array_merge($users,$query->as_array());
+                }
+
+                if (core::post('send_featured')=='on')
+                {
+                    $query = DB::select('email')->select('name')
+                            ->from(array('users','u'))
+                            ->join(array('ads','a'))
+                            ->using('id_user')
+                            ->where('a.status','=',Model_Ad::STATUS_PUBLISHED)
+                            ->where('a.featured','IS NOT',NULL)
+                            ->where('u.subscriber','=',1)
+                            ->group_by('id_user')
+                            ->execute();
+
+                    $users = array_merge($users,$query->as_array());
+                }
+
+                if (core::post('send_unpub')=='on')
+                {
+                     //users without published ads
+                    $query = DB::select('email')->select('name')
+                            ->from(array('users','u'))
+                            ->join(array('ads','a'),'LEFT')
+                            ->using('id_user')
+                            ->where('u.status','=',Model_User::STATUS_ACTIVE)
+                            ->where('u.subscriber','=',1)
+                            ->where('a.title','is',NULL)
+                            ->execute();
+
+                    $users = array_merge($users,$query->as_array());
+                }
+
+                if (core::post('send_logged')=='on')
+                {
+                    $query = DB::select('email')->select('name')
+                            ->from('users')
+                            ->where('status','=',Model_User::STATUS_ACTIVE)
+                            ->where('last_login','<=',Date::unix2mysql(strtotime('-3 month')))
+                            ->or_where('last_login','IS',NULL)
+                            ->where('subscriber','=',1)
+                            ->execute();
+
+                    $users = array_merge($users,$query->as_array());
+                }
+
+                if (core::post('send_spam')=='on')
+                {
+                    $query = DB::select('email')->select('name')
+                            ->from('users')
+                            ->where('status','=',Model_User::STATUS_SPAM)
+                            ->where('subscriber','=',1)
+                            ->execute();
+
+                    $users = array_merge($users,$query->as_array());
+                }
+            }
+
+            //NOTE $users may have duplicated emails, but phpmailer takes care of not sending the email 2 times to same recipient
+            
+            //sending!
+            if (count($users)>0)
             {
                 if ( !Email::send($users,'',Core::post('subject'),Core::post('description'),Core::post('from'), Core::post('from_email') ) )
                     Alert::set(Alert::ERROR,__('Error on mail delivery, not sent'));
                 else 
-                    Alert::set(Alert::SUCCESS,__('Email sent to all users'));
+                    Alert::set(Alert::SUCCESS,__('Email sent'));
             }
             else
             {
                 Alert::set(Alert::ERROR,__('Mail not sent'));
             }
 
-            
         }
 
-        
-
-
-
-        $this->template->content = View::factory('oc-panel/pages/newsletter',array('count'=>$user));
+        $this->template->content = View::factory('oc-panel/pages/newsletter',array( 'count_all_users'           => $count_all_users,
+                                                                                    'count_featured_expired'    => $count_featured_expired,
+                                                                                    'count_featured'    => $count_featured,
+                                                                                    'count_unpub'       => $count_unpub,
+                                                                                    'count_logged'      => $count_logged,
+                                                                                    'count_spam'        => $count_spam,
+                                                                                    )
+                                                                                );
 
     }
 
--- themes/default/views/oc-panel/pages/newsletter.php
+++ themes/default/views/oc-panel/pages/newsletter.php
@@ -2,13 +2,19 @@
 
 <?=View::factory('oc-panel/elasticemail')?>
 
+  <?if (Theme::get('premium')!=1):?>
+        <p class="well"><span class="label label-info"><?=__('Heads Up!')?></span> 
+            <?=__('Only if you have a premium theme you will be able to filter by users!').'<br/>'.__('Upgrade your Open Classifieds site to activate this feature.')?>
+            <a class="btn btn-success pull-right" href="<?=Route::url('oc-panel',array('controller'=>'theme'))?>"><?=__('Browse Themes')?></a>
+        </p>
+    <?endif?>
+
 <div class="page-header">
     <h1><?=__('Newsletter')?></h1>
   <a href="http://open-classifieds.com/2013/08/23/how-to-send-the-newsletter/" target="_blank"><?=__('Read more')?></a>
     <a class="btn btn-primary pull-right" href="<?=Route::url('oc-panel',array('controller'=>'settings','action'=>'email'))?>?force=1">
   <?=__('Email Settings')?></a>
-
-    </div>
+</div>
 
     <form class="well form-horizontal"  method="post" action="<?=Route::url('oc-panel',array('controller'=>'newsletter','action'=>'index'))?>">  
 
@@ -23,8 +29,36 @@
                         <?=__('All active users.')?> <span class="badge badge-info"><?=$count_all_users?></span>
                     </label>
                 </div> 
-                
-
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="send_featured"  >
+                        <?=__('Users with featured ads.')?> <span class="badge badge-info"><?=$count_featured?></span>
+                    </label>
+                </div> 
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="send_featured_expired"  >
+                        <?=__('Users with featured ads expired.')?> <span class="badge badge-info"><?=$count_featured_expired?></span>
+                    </label>
+                </div> 
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="send_unpub"  >
+                        <?=__('Users without published ads.')?> <span class="badge badge-info"><?=$count_unpub?></span>
+                    </label>
+                </div> 
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="send_logged"  >
+                        <?=__('Users not logged last 3 months')?> <span class="badge badge-info"><?=$count_logged?></span>
+                    </label>
+                </div> 
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="send_spam"  >
+                        <?=__('Users marked a spam')?> <span class="badge badge-info"><?=$count_spam?></span>
+                    </label>
+                </div> 
             </div> 
         </div>
        
