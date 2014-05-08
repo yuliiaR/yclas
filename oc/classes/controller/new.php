91c25c71241a06acc5414769fb96714a1c2c43d3
Slobodan <slobodan.josifovic@gmail.com>
2014-05-08 16:50:34 +0200

    in new === fix #667

--- oc/classes/controller/new.php
+++ oc/classes/controller/new.php
@@ -128,7 +128,7 @@ class Controller_New extends Controller
 	            if ($moderation == Model_Ad::POST_DIRECTLY) // direct post no moderation
 	            {
 
-	                if (Core::config('sitemap.on_post') === TRUE)
+	                if (Core::config('sitemap.on_post') == TRUE)
 	                    Sitemap::generate(); 
 
 	                $status = Model_Ad::STATUS_PUBLISHED;
