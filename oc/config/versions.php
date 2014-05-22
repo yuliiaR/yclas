49178171316b3552a938e4388f92b3492f3f9816
chema <neo22s@gmail.com>
2014-05-22 13:55:05 +0200

    closes #733 and closes #731

--- oc/config/versions.php
+++ oc/config/versions.php
@@ -88,7 +88,7 @@
   array (
     'codename' => 'Reoc',
     'released' => '2013-10-11',
-    'blog' => 'http://open-classifieds.com/2013/10/11/open-classifieds-2-0-7/',
+    'blog' => 'http://open-classifieds.com/2013/10/11/new-release-2-0-7/',
     'changelog' => 'https://github.com/open-classifieds/openclassifieds2/commits/2.0.7',
     'issues' => 'https://github.com/open-classifieds/openclassifieds2/issues?milestone=12&state=closed',
     'download' => 'http://j.mp/oc_207',
@@ -149,21 +149,5 @@
     'blog' => 'http://open-classifieds.com/2013/05/31/open-classifieds-2-0/ ',
     'changelog' => 'https://github.com/open-classifieds/openclassifieds2/commits/2.0',
     'issues' => 'https://github.com/open-classifieds/openclassifieds2/issues?milestone=5&state=closed',
-  ),
-  '2.0.RC1' => 
-  array (
-    'codename' => 'Reoc RC1',
-    'released' => '2013-05-17',
-    'blog' => 'http://open-classifieds.com/2013/05/17/open-classifieds-2-0-release-candidate/',
-    'changelog' => 'https://github.com/open-classifieds/openclassifieds2/commits/2.0.RC1',
-    'issues' => 'https://github.com/open-classifieds/openclassifieds2/issues?milestone=3&state=closed',
-  ),
-  '2.0.Beta1' => 
-  array (
-    'codename' => 'Reoc Beta1',
-    'released' => '2013-04-28',
-    'blog' => 'http://open-classifieds.com/2013/04/28/open-classifieds-2-0-beta-1/',
-    'changelog' => 'https://github.com/open-classifieds/openclassifieds2/commits/2.0.Beta1',
-    'issues' => 'https://github.com/open-classifieds/openclassifieds2/issues?milestone=1&state=closed',
   ),
 );
