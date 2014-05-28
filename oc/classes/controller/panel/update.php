429e1997ee0924889db416c7a139d99a6abe4595
chema <neo22s@gmail.com>
2014-05-28 13:17:27 +0200

    nah small fixes, refactor some code on widget pages

--- oc/classes/controller/panel/update.php
+++ oc/classes/controller/panel/update.php
@@ -582,9 +582,6 @@ class Controller_Panel_Update extends Auth_Controller {
         //delete file when all finished
         File::delete($update_src_dir);
 
-        //clean the cache so kohana file finder wont cache the search
-        Cache::instance()->delete_all();
-
         //update themes, different request so doesnt time out
         $this->request->redirect(Route::url('oc-panel', array('controller'=>'update', 'action'=>'themes','id'=>str_replace('.', '', $version)))); 
       
