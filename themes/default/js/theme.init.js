4e6ebd72a1bdd486f59280fb9aaf8ea47ae228a8
Oliver <oliver@open-classifieds.com>
2014-06-18 21:31:26 -0500

    Publish new images improvement #711

--- themes/default/js/theme.init.js
+++ themes/default/js/theme.init.js
@@ -37,6 +37,11 @@ $("button[name=submit]").click(function(){
         }
     });
 
+    //unhide next box image after selecting first
+	$('.fileinput').on('change.bs.fileinput', function() {
+	   $(this).next('.fileinput').removeClass('hidden');
+	});
+
     //online offline message
     window.addEventListener("offline", function(e) {
         $('.off-line').show();
