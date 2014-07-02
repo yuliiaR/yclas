d619fadfdca9f468c035efbdb2d697346d78897b
Oliver <oliver@open-classifieds.com>
2014-07-02 16:56:23 -0500

    Admin restyling #735

--- themes/default/views/oc-panel/header.php
+++ themes/default/views/oc-panel/header.php
@@ -61,7 +61,7 @@
 </header><!--/.navbar -->
 
 <?if (Auth::instance()->get_user()->id_role != Model_Role::ROLE_USER):?>
-<div class="col-sm-3 col-md-3 pull-right search-container-oc-faq">
+<div class="col-sm-3 col-md-3 hidden-xs pull-right search-container-oc-faq">
     <form class="navbar-form p_m0" role="search" action="http://open-classifieds.com/" target="_blank">
     <div class="input-group">
         <input type="text" class="form-control header-oc-faq" placeholder="<?=__('Search FAQs')?>" name="s" style="display: none;">
