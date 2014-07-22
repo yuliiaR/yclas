30141fa1b7caa13d1092b1831ee1faa29c0b9a75
Chema <neo22s@gmail.com>
2014-07-22 13:34:04 +0200

    improved install

--- install-openclassifieds.php
+++ install-openclassifieds.php
@@ -232,46 +232,54 @@ class core{
      * copies files/directories recursively
      * @param  string  $source    from
      * @param  string  $dest      to
-     * @param  boolean $overwrite overwrite existing file
+     * @param  integer $overwrite 0=do not overwrite 1=force overwrite 2=overwrite only is size is different
      * @return void             
      */
-    public static function copy($source, $dest, $overwrite = false)
+    public static function copy($source, $dest, $overwrite = 0)
     { 
-        //Lets just make sure our new folder is already created. Alright so its not efficient to check each time... bite me
-        if(is_file($dest))
+        //be sure source exists..
+        if (!is_readable($source))
+            die('File ('.$source.') could not be readed, likely a permissions problem.');
+
+        //just a file to copy, so do it!
+        if(is_file($source))
         {
-            copy($source, $dest);
+            $copy_file = FALSE;
+
+            //if doesnt exists OR we want to overwrite always OR different size copy the file.
+            if( !is_file( $dest ) OR $overwrite == 1 OR ( $overwrite == 2 AND filesize($source)===filesize($dest) ) ) 
+                $copy_file = TRUE;
+
+            if ($copy_file === TRUE)
+            {
+                try {
+                    copy($source, $dest);
+                } catch (Exception $e) {
+                    die('File ('.$source.') could not be copied, likely a permissions problem.');
+                }     
+            }
+            
+            //always return if its a file, so we dont move forward
             return;
         }
         
+        //was not a file, so folder...lets check exists, if not create it
         if(!is_dir($dest))
-            mkdir($dest);
+            mkdir($dest);     
 
+        //read folder contents
         $objects = scandir($source);
         foreach ($objects as $object) 
         {
             if($object != '.' && $object != '..')
             { 
-                $path = $source . '/' . $object; 
-                if(is_file( $path))
-                { 
-                    if(!is_file( $dest . '/' . $object) || $overwrite) 
-                    {
-                        if(!@copy( $path,  $dest . '/' . $object))
-                            die('File ('.$path.') could not be copied, likely a permissions problem.'); 
-                    }
-                }
-                elseif(is_dir( $path))
-                { 
-                    if(!is_dir( $dest . '/' . $object)) 
-                        mkdir( $dest . '/' . $object); // try to make subdirectory before subdirectory is copied 
-
-                    core::copy($path, $dest . '/' . $object, $overwrite); //recursive
-                }
-                 
+                $from = $source . '/' . $object; 
+                $to   = $dest   . '/' . $object;
+                Core::copy($from, $to, $overwrite);                  
             } 
         } 
-     }  
+        
+    }
 
     /**
      * recursively deletes file or directory
@@ -538,19 +546,24 @@ function __($msgid)
                     //download file
                     $file_content = core::curl_get_contents($versions[$last_version]['download']);
                     file_put_contents('oc.zip', $file_content);
-                    $fname = 'openclassifieds2-'.$last_version;
-
+                    
                     $zip = new ZipArchive;
                     // open zip file, extract to dir
                     if ($zip_open = $zip->open('oc.zip')) 
                     {
+                        if ( !($folder_update = $zip->getNameIndex(0)) )
+                        {
+                            hosting_view();
+                            exit;
+                        }
+
                         $zip->extractTo(DOCROOT);
                         $zip->close();  
                         
-                        core::copy($fname, DOCROOT);
+                        core::copy($folder_update, DOCROOT);
                         
                         // delete downloaded zip file
-                        core::delete($fname);
+                        core::delete($folder_update);
                         @unlink('oc.zip');
                         @unlink($_SERVER['SCRIPT_FILENAME']);
                         
