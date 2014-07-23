5bc602e1c9df8b964554235df598e8e453d1b076
Emmanuel <emanwebdev@gmail.com>
2014-07-23 23:40:40 +0200

    Added 2 help-blocks explanations

--- themes/default/views/oc-panel/pages/fields/new.php
+++ themes/default/views/oc-panel/pages/fields/new.php
@@ -80,7 +80,7 @@
                   <input type="checkbox" name="required"> 
                    <?=__('Required')?>
                 </label>
-            <div class="help-block"></div></div>
+            <div class="help-block col-xs-offset-4"><?=__('Required field to submit a new ad.')?></div></div>
         </div>
 
         <div class="form-group">
@@ -89,7 +89,7 @@
                   <input type="checkbox" name="searchable"> 
                    <?=__('Searchable')?>
                 </label>
-            <div class="help-block"></div></div>
+            <div class="help-block col-xs-offset-4"><?=__('Search in ads will include this field as well.')?></div></div>
         </div>
 
         <div class="form-group">
