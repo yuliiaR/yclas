f356debca40ecf2c7123c449fdc34cb49cf689b7
Oliver <oliver@open-classifieds.com>
2014-07-27 19:04:22 -0500

    Upload icon images for categories and locations.

--- oc/classes/model/category.php
+++ oc/classes/model/category.php
@@ -496,4 +496,16 @@ class Model_Category extends ORM {
         return $deep;
     }
 
+    /**
+     * reurns the url of the category icon
+     * @return string url
+     */
+    public function get_icon()
+    {
+		if(is_file(DOCROOT."images/categories/".$this->seoname.".png"))
+            return URL::base().'images/categories/'.$this->seoname.'.png';
+		else
+            return FALSE;
+    }
+
 } // END Model_Category
