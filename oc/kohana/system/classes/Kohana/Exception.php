eaac0b83d357d7e49d8b49ab5d3008416103ca85
Chema <neo22s@gmail.com>
2014-08-06 09:11:22 +0200

    closes #874

--- oc/kohana/system/classes/Kohana/Exception.php
+++ oc/kohana/system/classes/Kohana/Exception.php
@@ -1,3 +1,15 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
-class Kohana_Exception extends Kohana_Kohana_Exception {}
+class Kohana_Exception extends Kohana_Kohana_Exception {
+    /**
+     * Get a Response object representing the exception
+     *
+     * @uses    Kohana_Exception::text
+     * @param   Exception  $e
+     * @return  Response
+     */
+    public static function response(Exception $e)
+    {
+        d($e);
+    }
+}
