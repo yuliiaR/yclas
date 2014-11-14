9f1d38ffca994d1e6c91bb328ef12ef3abeecaa0
Oliver <oliverds@users.noreply.github.com>
2014-11-13 18:54:57 -0600

    Fontawesome 4.1.0 -> 4.2.0

--- themes/default/init.php
+++ themes/default/init.php
@@ -32,9 +32,8 @@ Theme::$skin = Theme::get('theme');
  * styles and themes, loaded in this order
  */
 
-Theme::$styles = array( '//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css' => 'screen',
-                        '//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css' => 'screen',
 Theme::$styles = array( '//netdna.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css' => 'screen',
+                        '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css' => 'screen',
                         '//cdn.jsdelivr.net/blueimp-gallery/2.14.0/css/blueimp-gallery.min.css' => 'screen', 
                         '//cdn.jsdelivr.net/bootstrap.datepicker/0.1/css/datepicker.css' => 'screen',
                         '//cdn.jsdelivr.net/chosen/1.0.0/chosen.css' => 'screen',
