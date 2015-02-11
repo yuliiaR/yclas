62469e953816f8999aa80ae4f3d08596250ac8ff
Oliver <oliverds@users.noreply.github.com>
2015-02-11 19:05:22 +0100

    JS auto-locations

--- themes/default/views/oc-panel/pages/locations/index.php
+++ themes/default/views/oc-panel/pages/locations/index.php
@@ -50,23 +50,84 @@
         </div>
     </div>
     <div class="col-md-5">
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <span class="label label-info"><?=__('Heads Up!')?> <?=__('Quick location creator.')?></span>
-                <div class="clearfix"></div> 
-                <?=__('Add names for multiple locations, for each one push enter.')?>
-                <div class="clearfix"></div><br>
-              
-                <?= FORM::open(Route::url('oc-panel',array('controller'=>'location','action'=>'multy_locations')), array('class'=>'form-inline', 'role'=>'form','enctype'=>'multipart/form-data'))?>
-                    <div class="form-group">
-                      <div class="">
-                        <?= FORM::label('multy_locations', __('Name').':', array('class'=>'control-label', 'for'=>'multy_locations'))?>
-                        <?= FORM::input('multy_locations', '', array('placeholder' => __('Hit enter to confirm'), 'class' => 'form-control', 'id' => 'multy_locations', 'type' => 'text','data-role'=>'tagsinput'))?>
-                        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <span class="label label-info"><?=__('Heads Up!')?> <?=__('Quick location creator.')?></span>
+                        <p><?=__('Add names for multiple locations, for each one push enter.')?></p>
+                      
+                        <?= FORM::open(Route::url('oc-panel',array('controller'=>'location','action'=>'multy_locations')), array('class'=>'form-horizontal', 'role'=>'form','enctype'=>'multipart/form-data'))?>
+                            <div class="form-group">
+                                <?= FORM::label('multy_locations', __('Name').':', array('class'=>'col-sm-3 control-label', 'for'=>'multy_locations'))?>
+                                <div class="col-sm-8">
+                                    <?= FORM::input('multy_locations', '', array('placeholder' => __('Hit enter to confirm'), 'class' => 'form-control', 'id' => 'multy_locations', 'type' => 'text','data-role'=>'tagsinput'))?>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-8">
+                                    <?= FORM::button('submit', __('Send'), array('type'=>'submit', 'class'=>'btn btn-primary', 'action'=>Route::url('oc-panel',array('controller'=>'location','action'=>'multy_locations'))))?>
+                                </div>
+                            </div>
+                        <?= FORM::close()?>
                     </div>
-                    <div class="clearfix"></div>
-                    <?= FORM::button('submit', __('Send'), array('type'=>'submit', 'class'=>'btn btn-primary', 'action'=>Route::url('oc-panel',array('controller'=>'location','action'=>'multy_locations'))))?>
-                <?= FORM::close()?>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <?= FORM::open(Route::url('oc-panel',array('controller'=>'location','action'=>'multy_locations')), array('id'=>'auto_locations_form', 'class'=>'form-horizontal', 'role'=>'form','enctype'=>'multipart/form-data'))?>
+                            <div class="form-group" id="group-continent">
+                                <label for="continent" class="col-sm-3 control-label" data-action="<?=__('Import continents')?>"><?=__('Continent')?></label>
+                                <div class="col-sm-8">
+                                    <select name="continent" id="continent" onchange="getPlaces(this.value,'country');" class="disable-chosen form-control">
+                                        <option value=""></option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group" id="group-country">
+                                <label for="country" class="col-sm-3 control-label" data-action="<?=__('Import countries')?>"><?=__('Country')?></label>
+                                <div class="col-sm-8">
+                                    <select name="country" id="country" onchange="getPlaces(this.value,'province');" class="disable-chosen form-control">
+                                        <option value=""></option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group" id="group-province">
+                                <label for="province" class="col-sm-3 control-label" data-action="<?=__('Import states/provinces')?>"><?=__('State')?> / <?=__('Province')?></label>
+                                <div class="col-sm-8">
+                                    <select name="province" id="province" onchange="getPlaces(this.value,'region');" class="disable-chosen form-control">
+                                        <option value=""></option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group" id="group-region">
+                                <label for="region" class="col-sm-3 control-label" data-action="<?=__('Import counties/regions')?>"><?=__('County')?> / <?=__('Region')?></label>
+                                <div class="col-sm-8">
+                                    <select name="region" id="region" onchange="getPlaces(this.value,'city');" class="disable-chosen form-control">
+                                        <option value=""></option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group" id="group-city">
+                                <label for="city" class="col-sm-3 control-label" data-action="<?=__('Import cities')?>"><?=__('City')?></label>
+                                <div class="col-sm-8">
+                                    <select name="city" id="city" class="disable-chosen form-control">
+                                        <option value=""></option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-8">
+                                    <input type="hidden" id="auto_locations" value="" name="multy_locations">
+                                    <input type="hidden" id="auto_locations_lang" value="<?=substr(Core::config('i18n.locale'), 0, -3)?>" name="auto_locations_lang">
+                                    <?= FORM::button('submit', __('Import'), array('type'=>'submit', 'class'=>'btn btn-primary', 'id'=>'auto_locations_import', 'action'=>Route::url('oc-panel',array('controller'=>'location','action'=>'multy_locations'))))?>
+                                    <?= FORM::button('reset', __('Reset'), array('type'=>'button', 'class'=>'btn btn-default', 'id'=>'auto_locations_import_reset'))?>
+                                </div>
+                            </div>
+                        <?= FORM::close()?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
