0b14e74da8d4424b18f7fe9e2d767df31043c7fb
Oliver <oliverds@users.noreply.github.com>
2015-03-24 15:31:44 +0100

    Improve categories/locations drag and drop

--- themes/default/views/oc-panel/pages/locations/index.php
+++ themes/default/views/oc-panel/pages/locations/index.php
@@ -14,37 +14,37 @@
             <div class="panel-heading"><?=$location->name?></div>
             <div class="panel-body">
                 <ol class='plholder' id="ol_<?=$location->id_location?>" data-id="<?=$location->id_location?>">
-                <?foreach ($locs as $loc){?>
-                
-                    <li data-id="<?=$loc->id_location?>" id="li_<?=$loc->id_location?>"><i class="glyphicon glyphicon-move"></i> <?=$loc->name?>
-                
-                        <div class="btn-group pull-right">
-                            <a class="btn btn-xs btn-info " title="<?=__('Browse childs')?>"
-                                href="<?=Route::url('oc-panel',array('controller'=>'location','action'=>'index'))?>?id_location=<?=$loc->id_location?>">
-                                <?=__('Browse childs')?>
-                            </a>
-
-                            <a class="btn btn-xs btn-primary ajax-load" title="<?=__('Edit')?>"
-                                href="<?=Route::url('oc-panel',array('controller'=>'location','action'=>'update','id'=>$loc->id_location))?>">
-                                <?=__('Edit')?>
-                            </a>
-
-                            <a 
-                                href="<?=Route::url('oc-panel', array('controller'=> 'location', 'action'=>'delete','id'=>$loc->id_location))?>" 
-                                class="btn btn-xs btn-danger index-delete index-delete-inline" 
-                                title="<?=__('Are you sure you want to delete? We will move the siblings locations and ads to the parent of this location.')?>" 
-                                data-id="li_<?=$loc->id_location?>" 
-                                data-placement="left" 
-                                data-href="<?=Route::url('oc-panel', array('controller'=> 'location', 'action'=>'delete','id'=>$loc->id_location))?>" 
-                                data-btnOkLabel="<?=__('Yes, definitely!')?>" 
-                                data-btnCancelLabel="<?=__('No way!')?>">
-                                <i class="glyphicon glyphicon-trash"></i>
-                            </a>
-                        </div>
-
-                    </li><!--li_<?=$loc->id_location?>-->
-                
-                <?}?>
+                    <?foreach ($locs as $loc) :?>
+                        <li data-id="<?=$loc->id_location?>" id="li_<?=$loc->id_location?>">
+                    
+                            <div class="drag-item">
+                                <span class="drag-icon"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span>
+                                <div class="drag-name">
+                                    <?=$loc->name?>
+                                </div>
+                                <a class="drag-action ajax-load" title="<?=__('Browse childs')?>"
+                                    href="<?=Route::url('oc-panel',array('controller'=>'location','action'=>'index'))?>?id_location=<?=$loc->id_location?>">
+                                    <?=__('Browse')?>
+                                </a>
+                                <a class="drag-action ajax-load" title="<?=__('Edit')?>"
+                                    href="<?=Route::url('oc-panel',array('controller'=>'location','action'=>'update','id'=>$loc->id_location))?>">
+                                    <i class="fa fa-pencil-square-o"></i>
+                                </a>
+                                <a 
+                                    href="<?=Route::url('oc-panel', array('controller'=> 'location', 'action'=>'delete','id'=>$loc->id_location))?>" 
+                                    class="drag-action index-delete" 
+                                    title="<?=__('Are you sure you want to delete? We will move the siblings locations and ads to the parent of this location.')?>" 
+                                    data-id="li_<?=$loc->id_location?>" 
+                                    data-placement="left" 
+                                    data-href="<?=Route::url('oc-panel', array('controller'=> 'location', 'action'=>'delete','id'=>$loc->id_location))?>" 
+                                    data-btnOkLabel="<?=__('Yes, definitely!')?>" 
+                                    data-btnCancelLabel="<?=__('No way!')?>">
+                                    <i class="glyphicon glyphicon-trash"></i>
+                                </a>
+                            </div>
+                    
+                        </li><!--li_<?=$loc->id_location?>-->
+                    <?endforeach?>
                 </ol><!--ol_1-->
                 
                 <span id='ajax_result' data-url='<?=Route::url('oc-panel',array('controller'=>'location','action'=>'saveorder'))?>'></span>
