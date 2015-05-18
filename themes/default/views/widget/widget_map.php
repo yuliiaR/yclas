3b93001a0b5a561547e56376eb5911587b0d6a5b
Oliver <oliverds@users.noreply.github.com>
2015-05-18 10:58:49 -0500

    closes #1240

--- themes/default/views/widget/widget_map.php
+++ themes/default/views/widget/widget_map.php
@@ -7,7 +7,6 @@
 <?endif?>
 
 <div class="panel-body">
-    <h3><?=$widget->map_title?></h3>
     <iframe frameborder="0" noresize="noresize" 
             height="<?=$widget->map_height+($widget->map_height*0.10)?>px" width="100%" 
             src="<?=Route::url('map')?>?height=<?=$widget->map_height?>&controls=0&zoom=<?=$widget->map_zoom?>">
