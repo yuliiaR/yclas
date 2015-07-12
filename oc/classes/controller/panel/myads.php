9bd21eb7dc6197a97a9056ba00db9b696d5e88de
Oliver <oliverds@users.noreply.github.com>
2015-07-11 22:22:03 -0500

    Edit Ad: load custom fields from API #1246

--- oc/classes/controller/panel/myads.php
+++ oc/classes/controller/panel/myads.php
@@ -319,10 +319,9 @@ class Controller_Panel_Myads extends Auth_Frontcontroller {
                             ->where('id_ad','=',$form->id_ad)->find_all();
 
             Breadcrumbs::add(Breadcrumb::factory()->set_title("Update"));
-            $this->template->content = View::factory('oc-panel/profile/edit_ad', array('ad'                 =>$form, 
-                                                                                       'extra_payment'      =>$extra_payment,
-                                                                                       'orders'             =>$orders,
-                                                                                       'fields'             => Model_Field::get_all()));
+            $this->template->content = View::factory('oc-panel/profile/edit_ad', array('ad'            => $form,
+                                                                                       'extra_payment' => $extra_payment,
+                                                                                       'orders'        => $orders));
 
 		}
 		else
