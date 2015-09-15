8523d1aaecb786599965ef81d1e92ad04391a6f2
Oliver <oliverds@users.noreply.github.com>
2015-09-15 11:28:55 -0500

    closes #1489: Moderators and translators access to oc-panel

--- oc/classes/controller/panel/home.php
+++ oc/classes/controller/panel/home.php
@@ -6,7 +6,7 @@ class Controller_Panel_Home extends Auth_Controller {
 	public function action_index()
 	{
         //if not god redirect him to the normal profile page
-        if (Auth::instance()->get_user()->id_role!=Model_Role::ROLE_ADMIN)
+        if ( ! in_array(Auth::instance()->get_user()->id_role, array(Model_Role::ROLE_ADMIN, Model_Role::ROLE_TRANSLATOR, Model_Role::ROLE_MODERATOR)))
             HTTP::redirect(Route::url('oc-panel',array('controller'  => 'myads','action'=>'index')));  
         
         Core::ocacu();
