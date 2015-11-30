916087d4a646ad9f81c4aae08551bafc393d51bb
chema <neo22s@gmail.com>
2015-11-30 18:16:27 +0100

    versions and es_ES updated

--- oc/config/versions.php
+++ oc/config/versions.php
@@ -3,8 +3,8 @@
   '2.6.0' => 
   array (
     'codename' => 'Taipei',
-    'released' => '2015-11-30',
-    'blog' => 'http://open-classifieds.com/2015/11/30/open-classifieds-2-6-0/',
+    'released' => '2015-12-01',
+    'blog' => 'http://open-classifieds.com/2015/12/01/open-classifieds-2-6-0/',
     'changelog' => 'https://github.com/open-classifieds/openclassifieds2/compare/2.5.1...2.6.0',
     'issues' => 'https://github.com/open-classifieds/openclassifieds2/issues?q=milestone%3A2.6.0+is%3Aclosed',
     'download' => 'http://j.mp/oc_260',
