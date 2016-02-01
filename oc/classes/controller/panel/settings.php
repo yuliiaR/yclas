1d0bfc30e2335231878a1524a1972f9660fafb80
chema <neo22s@gmail.com>
2016-02-01 10:32:23 +0100

    range of app_fee

--- oc/classes/controller/panel/settings.php
+++ oc/classes/controller/panel/settings.php
@@ -305,6 +305,7 @@ class Controller_Panel_Settings extends Auth_Controller {
             $validation =   Validation::factory($this->request->post())
             ->rule('pay_to_go_on_top', 'numeric')
             ->rule('stripe_appfee', 'numeric')
+            ->rule('stripe_appfee', 'range', array(':value', 0, 100))
             ->rule('to_featured', 'range', array(':value', 0, 1))
             ->rule('to_top', 'range', array(':value', 0, 1))
             ->rule('sandbox', 'range', array(':value', 0, 1))
