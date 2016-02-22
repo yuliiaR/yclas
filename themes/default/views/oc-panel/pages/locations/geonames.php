0d17cc91b652618cf5386fd7a6960743379c2ac8
Oliver <oliverds@users.noreply.github.com>
2016-02-21 22:45:29 -0600

    closes #1762: Geonames improvement

--- themes/default/views/oc-panel/pages/locations/geonames.php
+++ themes/default/views/oc-panel/pages/locations/geonames.php
@@ -53,6 +53,10 @@
                             </div>
                             <div class="form-group">
                                 <div class="col-sm-offset-3 col-sm-8">
+                                    <?if($location AND $location->id_geoname AND $location->fcodename_geoname):?>
+                                        <input type="hidden" id="current_location_id_geoname" value="<?=$location->id_geoname?>" name="current_location_id_geoname">
+                                        <input type="hidden" id="current_location_fcodename_geoname" value="<?=$location->fcodename_geoname?>" name="current_location_fcodename_geoname">
+                                    <?endif?>
                                     <input type="hidden" id="auto_locations" value="" name="geonames_locations">
                                     <input type="hidden" id="auto_locations_lang" value="<?=substr(Core::config('i18n.locale'), 0, -3)?>" name="auto_locations_lang">
                                     <?= FORM::button('submit', __('Import'), array('type'=>'submit', 'class'=>'btn btn-primary', 'id'=>'auto_locations_import', 'action'=>Route::url('oc-panel',array('controller'=>'location','action'=>'geonames')).'?id_location='.Core::get('id_location', 1)))?>
