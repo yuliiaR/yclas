05b341b2ab095a13bbc08d0bbd438bb742df98b2
chema <neo22s@gmail.com>
2016-06-22 12:09:14 +0200

    new config for cache #1926

--- oc/config/cache.php
+++ oc/config/cache.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 return array
 (
-    'default' => 'file',
+    'default' => 'memcache',
     
     'file'  => array
     (
@@ -15,4 +15,28 @@ return array
         'driver'             => 'apc',
         'default_expire'     => 3600,
     ),
+
+    'apcu'      => array(
+        'driver'             => 'apcu',
+        'default_expire'     => 3600,
+    ),
+
+    'memcache' => array
+    (
+        'driver'             => 'memcache',
+        'default_expire'     => 3600,
+        'compression'        => FALSE,              // Use Zlib compression 
+                                                    // (can cause issues with integers)
+        'servers'            => array
+        (
+            'local' => array
+            (
+                'host'             => 'localhost',  // Memcache Server
+                'port'             => 11211,        // Memcache port number
+                'persistent'       => FALSE,        // Persistent connection
+            ),
+        ),
+        'instant_death'      => TRUE, 
+    ),
+
 );
