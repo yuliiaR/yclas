c647699367a98763f2ae72325d9cf49bd55123ce
chema <neo22s@gmail.com>
2016-10-03 11:32:00 +0200

    fixes mercadopago

--- oc/classes/controller/mercadopago.php
+++ oc/classes/controller/mercadopago.php
@@ -32,6 +32,13 @@ class Controller_MercadoPago extends Controller{
 
         if ($order->loaded())
         {
+            //its a fraud...lets let him know
+            if ( $order->is_fraud() === TRUE )
+            {
+                Kohana::$log->add(Log::ERROR,  __('We had, issues with your transaction. Please try paying with another paymethod.'));
+                $this->response->body('KO');
+            }
+
             // Include Mercadopago library
             require Kohana::find_file('vendor/mercadopago', 'mercadopago');
 
@@ -41,7 +48,7 @@ class Controller_MercadoPago extends Controller{
             $params = ["access_token" => $mp->get_access_token()];
 
             // Check mandatory parameters
-            if (!isset(Core::get('id'), Core::get('topic') || !ctype_digit(Core::get('id'))) 
+            if ( Core::get('id') == NULL OR Core::get('topic') == NULL OR !ctype_digit(Core::get('id')) ) 
             {
                 $this->response->body('KO');
             }
@@ -49,17 +56,33 @@ class Controller_MercadoPago extends Controller{
             // Get the payment reported by the IPN. Glossary of attributes response in https://developers.mercadopago.com
             if(Core::get('topic') == 'payment')
             {
-                $payment_info = $mp->get("/collections/notifications/" . Core::get('id'), $params, false);
-                $merchant_order_info = $mp->get("/merchant_orders/" . $payment_info["response"]["collection"]["merchant_order_id"], $params, false);
-                // Get the merchant_order reported by the IPN. Glossary of attributes response in https://developers.mercadopago.com    
+                try {
+                    $payment_info = $mp->get("/collections/notifications/" . Core::get('id'), $params, false);
+                    // Get the merchant_order reported by the IPN. Glossary of attributes response in https://developers.mercadopago.com    
+                } catch (Exception $e) {
+                    Kohana::$log->add(Log::ERROR, $e);
+                    $this->response->body('KO');
+                }
+
+                try {
+                    $merchant_order_info = $mp->get("/merchant_orders/" . $payment_info["response"]["collection"]["merchant_order_id"], $params, false);  
+                } catch (Exception $e) {
+                    Kohana::$log->add(Log::ERROR, $e);
+                    $this->response->body('KO');
+                }
             }
             else if(Core::get('topic') == 'merchant_order')
             {
-                $merchant_order_info = $mp->get("/merchant_orders/" . Core::get('id'), $params, false);
+                try {
+                    $merchant_order_info = $mp->get("/merchant_orders/" . Core::get('id'), $params, false);
+                } catch (Exception $e) {
+                    Kohana::$log->add(Log::ERROR, 'Order not loaded');
+                    $this->response->body('KO');
+                }
             }
 
             //If the payment's transaction amount is equal (or bigger) than the merchant order's amount you can release your items 
-            if ($merchant_order_info["status"] == 200) 
+            if (isset($merchant_order_info["status"]) AND $merchant_order_info["status"] == 200) 
             {
                 $transaction_amount_payments= 0;
                 $transaction_amount_order = $merchant_order_info["response"]["total_amount"];
