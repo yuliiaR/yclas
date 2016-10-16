658eb9a51dc081ac6c972c54d9712f2e14536904
Emmanuel <emanwebdev@users.noreply.github.com>
2016-10-16 03:49:05 +0200

    Presentation improvement
    Added < br > to improve message readability

--- themes/default/views/oc-panel/pages/tools/import_tool.php
+++ themes/default/views/oc-panel/pages/tools/import_tool.php
@@ -14,9 +14,9 @@
             </div>
             <div class="panel-body">
                 <p>
-                    <?=__('Please use the correct CSV format')?>, <?=__('limited to 10.000 at a time')?>, <?=__('1 MB file')?>.
+                    <?=__('Please use the correct CSV format')?>,<br><?=__('limited to 10.000 at a time')?>,<br><?=__('1 MB file')?>.
                     <br>
-                    <?=__('Categories')?>: <a href="https://mega.co.nz/#!4wZQVCzA!EHYk1TdDgBr6U600eS28rtgbTfIYzONwmzpb7o3pIhg"><?=__('download example')?>.</a>
+                    <?=__('Categories')?>: <a href="https://mega.co.nz/#!4wZQVCzA!EHYk1TdDgBr6U600eS28rtgbTfIYzONwmzpb7o3pIhg"><?=__('download example')?>.</a><br>
                     <?=__('Locations')?>: <a href="https://mega.co.nz/#!BxZDzDCC!ANjH-uKxKhcaYwI2_xy0QQWVtvWK510ObGLAaUp_rWQ"><?=__('download example')?>.</a>
                 </p>
                 <hr>
