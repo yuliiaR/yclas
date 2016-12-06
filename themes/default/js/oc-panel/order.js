51afd7f4ce23549e6bf41db0fa8f47d26bf8504e
Oliver <oliverds@users.noreply.github.com>
2016-12-06 10:59:41 -0600

    Bootstrap for order print, related to #2171

--- themes/default/js/oc-panel/order.js
+++ themes/default/js/oc-panel/order.js
@@ -3,5 +3,8 @@ $(function(){
     window.document.write('<html><body>');
     window.document.write(divContents);
     window.document.write('</body></html>');
+    $('body').css({"margin-top": "30px"});
+    $('body').wrapInner('<div class="container"></div>');
+    $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '//cdn.jsdelivr.net/bootstrap/3.3.6/css/bootstrap.css') );
     window.print();
 });
