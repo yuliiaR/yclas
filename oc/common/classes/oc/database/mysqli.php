2048d8c7f8f8fc969499fe087645829af531f5f0
chema <neo22s@gmail.com>
2017-01-25 16:50:13 +0100

    moving common https://github.com/yclas/yclas/issues/2227

--- oc/common/classes/oc/database/mysqli.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class OC_Database_MySQLi extends Kohana_Database_MySQLi {}
--- oc/common/classes/oc/image/gd.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-class OC_Image_GD extends Kohana_Image_GD {}
