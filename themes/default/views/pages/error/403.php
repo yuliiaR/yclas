b4c42face5ff488a8f75b047796cd100b7d9a8c4
cybernet <cybernet2u@yahoo.com>
2017-03-03 10:22:23 +0200

    don't index

--- themes/default/views/pages/error/403.php
+++ themes/default/views/pages/error/403.php
@@ -7,7 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Yclas">
+    <meta name="robots" content="noindex">
+    <meta name="googlebot" content="noindex">
 
     <title><?=Core::config('general.site_name')?> - <?=__('Private Site')?></title>
 
