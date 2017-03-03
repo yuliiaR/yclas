f6b0c120da268050d02d099ec5e38f919c29f700
cybernet <cybernet2u@yahoo.com>
2017-03-03 10:21:52 +0200

    don't index

--- themes/default/views/pages/error/default.php
+++ themes/default/views/pages/error/default.php
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Yclas">
+    <meta name="robots" content="noindex">
+    <meta name="googlebot" content="noindex">
 
     <title>An error :'(</title>
 
