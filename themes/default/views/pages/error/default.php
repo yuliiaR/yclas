87868c3e51f48185be44c3e2f0629900f6988581
cybernet <cybernet2u@yahoo.com>
2017-03-03 09:02:29 +0200

    bootstrap 3.37 & meta author = Yclas

--- themes/default/views/pages/error/default.php
+++ themes/default/views/pages/error/default.php
@@ -7,11 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Yclas">
 
     <title>An error :'(</title>
 
-    <link href="//cdn.jsdelivr.net/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//cdn.jsdelivr.net/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
   </head>
 
