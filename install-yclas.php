5c86e2702f869efaa1e9194852bcd3a79f530241
chema <neo22s@gmail.com>
2017-03-28 11:35:07 +0200

    3.2.0 version

--- install-yclas.php
+++ install-yclas.php
@@ -52,7 +52,7 @@ class install{
      * Software install settings
      * @var string
      */
-    const VERSION   = '3.1.0';
+    const VERSION   = '3.2.0';
 
     /**
      * default locale/language of the install
