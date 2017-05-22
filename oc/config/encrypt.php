b67cb9ea26262efdef9628f0ad40ed0464d20a49
chema <neo22s@gmail.com>
2017-05-22 13:31:25 +0200

    closes https://github.com/yclas/yclas/issues/2176

---
+++ oc/config/encrypt.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'default' => array(
+		'type'   => 'openssl',
+        'key'    => '1234567891234567',//not in use, we use auth.php hash_key
+		'cipher' => 'AES-128-CBC',
+		'mode'   => MCRYPT_MODE_NOFB,
+	),
+
+);
