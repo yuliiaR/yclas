be742ca2345067c7179ba8578690432f8188d16d
kotsios5 <pantazis.constantinos@gmail.com>
2017-06-21 13:52:11 +0100

    improve share widget - add title + more social media

--- themes/default/views/widget/widget_share.php
+++ themes/default/views/widget/widget_share.php
@@ -1,5 +1,12 @@
+<?if ($widget->text_title!=''):?>
+    <div class="panel-heading">
+        <h3 class="panel-title"><?=$widget->text_title?></h3>
+    </div>
+<?endif?>
+
 <?if($widget->social_media==''):?>
-	<span class='st_whatsapp_large' displayText='WhatsApp'></span>
+	<span class='st_whatsapp_large' displayText='WhatsApp'></span> <!-- only on mobile -->
+	<span class='st_sms_large' displayText='SMS'></span> <!-- only on mobile -->
 	<span class='st_facebook_large' displayText='Facebook'></span>
 	<span class='st_twitter_large' displayText='Tweet'></span>
 	<span class='st_linkedin_large' displayText='LinkedIn'></span>
@@ -7,18 +14,30 @@
 	<span class='st_googleplus_large' displayText='Google +'></span>
 	<span class='st_vkontakte_large' displayText='Vkontakte'></span>
 	<span class='st_odnoklassniki_large' displayText='Odnoklassniki'></span>
+	<span class='st_tumblr_large' displayText='Tumblr'></span>
+	<span class='st_reddit_large' displayText='Reddit'></span>
+	<span class='st_stumbleupon_large' displayText='Stumbleupon'></span>
+	<span class='st_digg_large' displayText='Digg'></span>
+	<span class='st_flipboard_large' displayText='Flipboard'></span>
 	<span class='st_email_large' displayText='Email'></span>
 	<span class='st_print_large' displayText='Print'></span>
 <?else:?>
 	<? $social_media = explode(',', $widget->social_media); ?>
 	<?foreach($social_media as $key=>$value):?>
 		<?if(!strcmp(trim($value),'whatsapp')):?><span class='st_whatsapp_large' displayText='WhatsApp'></span><?endif?>
+		<?if(!strcmp(trim($value),'sms')):?><span class='st_sms_large' displayText='SMS'></span><?endif?>
 		<?if(!strcmp(trim($value),'facebook')):?><span class='st_facebook_large' displayText='Facebook'></span><?endif?>
 		<?if(!strcmp(trim($value),'tweet')):?><span class='st_twitter_large' displayText='Tweet'></span><?endif?>
 		<?if(!strcmp(trim($value),'linkedin')):?><span class='st_linkedin_large' displayText='LinkedIn'></span><?endif?>
 		<?if(!strcmp(trim($value),'pinterest')):?><span class='st_pinterest_large' displayText='Pinterest'></span><?endif?>
 		<?if(!strcmp(trim($value),'googleplus')):?><span class='st_googleplus_large' displayText='Google +'></span><?endif?>
+		<?if(!strcmp(trim($value),'vkontakte')):?><span class='st_vkontakte_large' displayText='Vkontakte'></span><?endif?>
 		<?if(!strcmp(trim($value),'odnoklassniki')):?><span class='st_odnoklassniki_large' displayText='Odnoklassniki'></span><?endif?>
+		<?if(!strcmp(trim($value),'tumblr')):?><span class='st_tumblr_large' displayText='Tumblr'></span><?endif?>
+		<?if(!strcmp(trim($value),'reddit')):?><span class='st_reddit_large' displayText='Reddit'></span><?endif?>
+		<?if(!strcmp(trim($value),'stumbleupon')):?><span class='st_stumbleupon_large' displayText='Stumbleupon'></span><?endif?>
+		<?if(!strcmp(trim($value),'digg')):?><span class='st_digg_large' displayText='Digg'></span><?endif?>
+		<?if(!strcmp(trim($value),'flipboard')):?><span class='st_flipboard_large' displayText='Flipboard'></span><?endif?>
 		<?if(!strcmp(trim($value),'email')):?><span class='st_email_large' displayText='Email'></span><?endif?>
 		<?if(!strcmp(trim($value),'print')):?><span class='st_print_large' displayText='Print'></span><?endif?>
 	<?endforeach?>
