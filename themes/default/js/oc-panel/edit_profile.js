2fccb53dc29fd543e930465135a5924b891b08bf
Oliver <oliverds@users.noreply.github.com>
2017-08-25 15:20:43 -0500

    Fixes intlTelInput on phone login

--- themes/default/js/oc-panel/edit_profile.js
+++ themes/default/js/oc-panel/edit_profile.js
@@ -3,11 +3,14 @@ if($('.cf_date_fields').length != 0){
     $('.cf_date_fields').datepicker();
 }
 
-$('#phone').intlTelInput({
-    formatOnDisplay: false,
-    initialCountry: $('#phone').data('country')
-});
+if ($('input[name="phone"]').length != 0) {
+    $('input[name="phone"]').intlTelInput({
+        formatOnDisplay: false,
+        initialCountry: $('input[name="phone"]').data('country')
+    });
 
-$('form').submit(function() {
-    $('#phone').val($('#phone').intlTelInput("getNumber"));
-});
+    $('form').submit(function() {
+        var $phoneField = $(this).find('input[name="phone"]');
+        $phoneField.val($phoneField.intlTelInput("getNumber"));
+    });
+}
