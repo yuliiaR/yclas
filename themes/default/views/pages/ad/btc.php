f091ebc0c86e648c48d515815d79cabcfbb88ed4
kotsios5 <pantazis.constantinos@gmail.com>
2018-01-08 14:08:20 +0000

    closes to #2576

--- themes/default/views/pages/ad/btc.php
+++ themes/default/views/pages/ad/btc.php
@@ -2,12 +2,16 @@
 	<b><?=_e('Bitcoin Address')?></b>
 	<br>
 	<?if(isset($ad->cf_bitcoinaddress) AND !empty($ad->cf_bitcoinaddress)):?>
-		<img title="Bitcoin QR code for the address <?=$ad->cf_bitcoinaddress?>" src="https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=<?=$ad->cf_bitcoinaddress?>">	
+		<a href="bitcoin:<?=$ad->cf_bitcoinaddress?>">
+			<img title="Bitcoin QR code for the address <?=$ad->cf_bitcoinaddress?>" src="https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=<?=$ad->cf_bitcoinaddress?>">	
+		</a>
 		<br>
-		<b><?=$ad->cf_bitcoinaddress?></b>
+		<a href="bitcoin:<?=$ad->cf_bitcoinaddress?>"><b><?=$ad->cf_bitcoinaddress?></b></a>
 	<?elseif(isset($ad->user->cf_bitcoinaddress) AND !empty($ad->user->cf_bitcoinaddress)):?>
-		<img title="Bitcoin QR code for the address <?=$ad->user->cf_bitcoinaddress?>" src="https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=<?=$ad->user->cf_bitcoinaddress?>">	
+		<a href="bitcoin:<?=$ad->user->cf_bitcoinaddress?>">
+			<img title="Bitcoin QR code for the address <?=$ad->user->cf_bitcoinaddress?>" src="https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=<?=$ad->user->cf_bitcoinaddress?>">	
+		</a>
 		<br>
-		<b><?=$ad->user->cf_bitcoinaddress?></b>
+		<a href="bitcoin:<?=$ad->user->cf_bitcoinaddress?>"><b><?=$ad->user->cf_bitcoinaddress?></b></a>
 	<?endif?>
 </p>
