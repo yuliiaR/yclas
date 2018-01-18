e923006a2e780e7db4b805900603d0dea78c9da7
kotsios5 <pantazis.constantinos@gmail.com>
2018-01-18 15:04:24 +0000

    closes #2609

--- themes/default/views/widget/widget_currency.php
+++ themes/default/views/widget/widget_currency.php
@@ -1,12 +1,28 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
-
-<?if ($widget->currency_title!=''):?>
-    <div class="panel-heading">
-        <h3 class="panel-title"><?=$widget->currency_title?></h3>
-    </div>
+<?if(array_key_exists('currency', Model_Field::get_all()) AND (strtolower(Request::current()->controller())=='ad' AND strtolower(Request::current()->action())=='view')):?>
+	<?if ($widget->currency_title!=''):?>
+	    <div class="panel-heading">
+	        <h3 class="panel-title"><?=$widget->currency_title?></h3>
+	    </div>
+	<?endif?>
+	<div class="panel-body">
+		<?if(isset(Model_Ad::current()->cf_currency) AND !empty(Model_Ad::current()->cf_currency)):?>
+	    	<div class="form-group curry" data-locale="<?=Model_Ad::current()->currency();?>" data-currencies="<?=($widget->currencies);?>" data-default="<?=($widget->default);?>">
+	    <?else:?>
+	    	<div class="form-group curry" data-locale="<?=core::config('general.number_format');?>" data-currencies="<?=($widget->currencies);?>" data-default="<?=($widget->default);?>">
+	    <?endif?>
+	        <div class="my-future-ddm"></div>
+	    </div>
+	</div>
+<?elseif((!array_key_exists('currency', Model_Field::get_all()))):?>
+	<?if ($widget->currency_title!=''):?>
+	    <div class="panel-heading">
+	        <h3 class="panel-title"><?=$widget->currency_title?></h3>
+	    </div>
+	<?endif?>
+	<div class="panel-body">
+    	<div class="form-group curry" data-locale="<?=core::config('general.number_format');?>" data-currencies="<?=($widget->currencies);?>" data-default="<?=($widget->default);?>">
+	        <div class="my-future-ddm"></div>
+	    </div>
+	</div>
 <?endif?>
-<div class="panel-body">
-    <div class="form-group curry" data-locale="<?=Core::config('i18n.locale');?>" data-currencies="<?=($widget->currencies);?>" data-default="<?=($widget->default);?>">
-        <div class="my-future-ddm"></div>
-    </div>
-</div>
