8d70172400ea7f4f433d895ef10fe954317e5ff5
kotsios5 <pantazis.constantinos@gmail.com>
2018-01-30 19:16:55 +0000

    yclas/yclas for #2617

--- themes/default/views/pages/ad/checkout.php
+++ themes/default/views/pages/ad/checkout.php
@@ -7,18 +7,27 @@
                 <strong><?=Core::config('general.site_name')?></strong>
                 <br>
                 <?=Core::config('general.base_url')?>
+                <br>
+	            <?if(isset($order->VAT) AND $order->VAT > 0):?>
+	                <em><?=_e('VAT Number')?>: <?=$order->VAT_country?> <?=$order->VAT_number?></em>
+	                <br>
+	            <?endif?>
+	            <em><?=_e('Date')?>: <?= Date::format($order->created, core::config('general.date_format'))?></em>
+	            <br>
+	            <em><?=_e('Checkout')?> #: <?=$order->id_order?></em>
             </address>
         </div>
         <div class="col-xs-6 col-sm-6 col-md-6 text-right">
-            <p>
-                <?if(isset($order->VAT) AND $order->VAT > 0):?>
-                    <em><?=_e('VAT Number')?>: <?=$order->VAT_country?> <?=$order->VAT_number?></em>
-                    <br>
-                <?endif?>
-                <em><?=_e('Date')?>: <?= Date::format($order->created, core::config('general.date_format'))?></em>
-                <br>
-                <em><?=_e('Checkout')?> :# <?=$order->id_order?></em>
-            </p>
+            <address>
+            	<strong><?=$order->user->name?></strong>
+            	<br>
+            	<span><?=$order->user->email?></span>
+            	<br>
+            	<?if($order->user->address != NULL):?>
+	            	<span><?=$order->user->address?></span>
+	            	<br>
+            	<?endif?>
+            </address>
         </div>
     </div>
     <div class="row">
