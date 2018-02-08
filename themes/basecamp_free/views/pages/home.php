6ad1e744db81bf99b70a0e13ad8764da5b6ff9d7
kotsios5 <pantazis.constantinos@gmail.com>
2018-02-08 16:44:55 +0000

    related to #2622

--- themes/basecamp_free/views/pages/home.php
+++ themes/basecamp_free/views/pages/home.php
@@ -20,15 +20,7 @@
 <!-- // INDEX HEAD -->
 
 <?if(core::config('advertisement.homepage_map') == 1):?>
-	<iframe
-		width="100%"
-		height="<?=(empty(core::config('advertisement.homepage_map_height'))?'400px':core::config('advertisement.homepage_map_height'))?>" 
-		frameBorder="0"
-		src="/map.html"
-		class="homepage_map color-section"
-		<?=((core::config('advertisement.homepage_map_allowfullscreen')==1)?'allowfullscreen':'')?>
-		>
-	</iframe>
+	<?=View::factory('pages/map/home')?>
 <?endif?>
 
 <!-- MAIN CONTENT - ADS -->
@@ -156,15 +148,7 @@
 </div>
 
 <?if(core::config('advertisement.homepage_map') == 2):?>
-	<iframe
-		width="100%"
-		height="<?=(empty(core::config('advertisement.homepage_map_height'))?'400px':core::config('advertisement.homepage_map_height'))?>" 
-		frameBorder="0"
-		src="/map.html"
-		class="homepage_map color-section"
-		<?=((core::config('advertisement.homepage_map_allowfullscreen')==1)?'allowfullscreen':'')?>
-		>
-	</iframe>
+	<?=View::factory('pages/map/home')?>
 <?endif?>
 
 <div id="modalAllCategories" class="modal fade" tabindex="-1" data-apiurl="<?=Route::url('api', array('version'=>'v1', 'format'=>'json', 'controller'=>'categories'))?>">
