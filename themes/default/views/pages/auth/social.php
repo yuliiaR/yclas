9faa0039c4d55b1f586333d5028e112f7881a947
Oliver <oliverds@users.noreply.github.com>
2018-02-19 10:35:37 -0600

    closes #2635: OAuth login

--- themes/default/views/pages/auth/social.php
+++ themes/default/views/pages/auth/social.php
@@ -1,7 +1,7 @@
 <?if (Theme::get('premium')==1):?>
-    <?if (core::count($providers = Social::enabled_providers()) > 0) :?>
+    <?if (core::count($providers = Social::enabled_providers()) > 0 OR core::config('social.oauth2_enabled') == TRUE) :?>
         <ul class="list-inline social-providers">
-            <?foreach ($providers as $key => $provider) :?>     
+            <?foreach ($providers as $key => $provider) :?>
                 <li>
                     <?if(strtolower($key) == 'live' OR strtolower($key) == 'vkontakte'):?>
                         <a class="zocial  <?=strtolower($key) == 'live' ? 'windows' : 'vk'?>" href="<?=Route::url('default',array('controller'=>'social','action'=>'login','id'=>strtolower($key)))?>">
@@ -14,6 +14,13 @@
                     <?endif?>
                 </li>
             <?endforeach?>
+            <?if (core::config('social.oauth2_enabled') == TRUE):?>
+                <li>
+                    <a class="zocial secondary" href="<?=Route::url('default',array('controller'=>'social','action'=>'oauth','id'=>1))?>">
+                        <?=__('OAuth')?>
+                    </a>
+                </li>
+            <?endif?>
         </ul>
     <?endif?>
 <?endif?>
