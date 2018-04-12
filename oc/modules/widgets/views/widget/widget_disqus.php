e5514c1462f107d79e731927c61c1704caa8b495
chema <neo22s@gmail.com>
2018-04-12 08:19:05 +0200

    disqus

--- oc/modules/widgets/views/widget/widget_disqus.php
+++ oc/modules/widgets/views/widget/widget_disqus.php
@@ -8,6 +8,6 @@
 
 <div class="panel-body">
     <script type="text/javascript" 
-    src="http://disqus.com/forums/<?=core::config('advertisement.disqus')?>/combination_widget.js?num_items=<?=$widget->comments_limit?>&hide_mods=0&default_tab=recent&excerpt_length=200">
+    src="https://disqus.com/forums/<?=core::config('advertisement.disqus')?>/combination_widget.js?num_items=<?=$widget->comments_limit?>&hide_mods=0&default_tab=recent&excerpt_length=200">
     </script>
 </div>
